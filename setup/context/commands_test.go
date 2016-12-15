cffbd32aca70dbfa1e6c15363a06b65a7ccfd8ea
Jonathan Stallings <jonathanstallings@outlook.com>
2016-12-15 08:13:32 -0800

    Improve mock object for unit test

--- setup/context/commands_test.go
+++ setup/context/commands_test.go
@@ -1,48 +1,55 @@
 package context
 
 import (
+	"fmt"
 	"os"
 	"testing"
 )
 
 type MockFileIO struct {
-	data []byte
+	data map[string][]byte
 }
 
 func (mock MockFileIO) ReadFile(path string) ([]byte, error) {
-	return mock.data, nil
+	if contents, ok := mock.data[path]; ok {
+		return contents, nil
+	}
+	return nil, fmt.Errorf("Failed to read file at path: `%s`", path)
 }
 
 func (mock MockFileIO) WriteFile(path string, data []byte, perm os.FileMode) error {
-	mock.data = data
+	mock.data[path] = data
 	return nil
 }
 
 func (mock MockFileIO) Stat(path string) (os.FileInfo, error) {
-	return nil, nil
+	if _, ok := mock.data[path]; ok {
+		return nil, nil
+	}
+	return nil, fmt.Errorf("Failed to stat file at path: `%s`", path)
 }
 
 func TestValidateDockercfgWithValidData(t *testing.T) {
-	mockIO := MockFileIO{data: []byte(`
+	mockIO := MockFileIO{data: map[string][]byte{"foo": []byte(`
         {
             "https://d.ims.io": {
                 "auth": "StopLookingAtMySecretsYouJerk=",
                 "email": ""
             }
-        }`)}
+        }`)}}
 	if err := validateDockercfg("foo", mockIO); err != nil {
 		t.Fatal(err)
 	}
 }
 
 func TestValidateDockercfgWithInvalidData(t *testing.T) {
-	mockIO := MockFileIO{data: []byte(`
+	mockIO := MockFileIO{data: map[string][]byte{"foo": []byte(`
         {
             "https://d.ims.io": {
                 "auth": "StopLookingAtMySecretsYouJerk=",
                 "email": ""
             },
-        }`)}
+        }`)}}
 	if err := validateDockercfg("foo", mockIO); err == nil {
 		t.Fatal(err)
 	}
