0957bc71ebfdc739b56c1214d6117d3cdef96cc7
Ivers, Brandon (Seattle) <bivers@us.imshealth.com>
2017-04-21 13:04:15 -0700

    fixes based on PR feedback

--- plugins/terraform/resource_environment_link_test.go
+++ plugins/terraform/resource_environment_link_test.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"errors"
-	"log"
 	"testing"
 
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/quintilesims/layer0/common/models"
 )
 
-func TestEnvironmentLink_defaults(t *testing.T) {
+func TestEnvironmentLinkCreate(t *testing.T) {
 	ctrl, mockClient, provider := setupUnitTest(t)
 	defer ctrl.Finish()
 
@@ -32,35 +31,48 @@ func TestEnvironmentLink_defaults(t *testing.T) {
 	}
 }
 
+func TestEnvironmentLinkRead(t *testing.T) {
+	ctrl, mockClient, provider := setupUnitTest(t)
+	defer ctrl.Finish()
+
+	mockClient.EXPECT().
+		GetEnvironment("test-env").
+		Return(&models.Environment{}, nil)
+
+	environmentResource := provider.ResourcesMap["layer0_environment_link"]
+	d := schema.TestResourceDataRaw(t, environmentResource.Schema, map[string]interface{}{
+		"source": "test-env",
+		"dest":   "test-env2",
+	})
+
+	if err := environmentResource.Read(d, mockClient); err != nil {
+		t.Fatal(err)
+	}
+}
+
 func TestEnvironmentLinkRead_noenv(t *testing.T) {
 	ctrl, mockClient, provider := setupUnitTest(t)
 	defer ctrl.Finish()
 
 	mockClient.EXPECT().
 		GetEnvironment("test-env").
-		Return(&models.Environment{}, errors.New("No environment found"))
+		Return(nil, errors.New("No environment found"))
 
 	environmentResource := provider.ResourcesMap["layer0_environment_link"]
 	d := schema.TestResourceDataRaw(t, environmentResource.Schema, map[string]interface{}{
 		"source": "test-env",
-		"dest":   "",
+		"dest":   "test-env2",
 	})
 
 	if err := environmentResource.Read(d, mockClient); err != nil {
 		t.Fatal(err)
 	}
-	// not sure if checking the ID is needed here as the other client calls already
-	// catch our desired behavior and print to stdout
-	// ex:
-	// 2017/04/20 14:34:45 [WARN] Error Reading Environment Link (test-env), link does not exist
-	// 2017/04/20 14:34:45 [WARN] Error Reading Environment (test-env), environment does not exist
-
-	if d.Id() == "" {
-		log.Printf("[WARN] Resource does not exist (%s)", "test-env")
+	if d.Id() != "" {
+		t.Fatal("Id should be set to empty string")
 	}
 }
 
-func TestEnvironmentLinkDelete_default(t *testing.T) {
+func TestEnvironmentLinkDelete(t *testing.T) {
 	ctrl, mockClient, provider := setupUnitTest(t)
 	defer ctrl.Finish()
 
