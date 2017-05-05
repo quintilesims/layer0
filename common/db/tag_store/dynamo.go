86386bfbe4830d81e0491f5036dd3700e6f10106
zpatrick <zpatrick319@hotmail.com>
2017-05-05 17:59:31 +0000

    add some batch testing

--- common/db/tag_store/dynamo.go
+++ common/db/tag_store/dynamo.go
@@ -1,95 +1,149 @@
 package tag_store
 
 import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/guregu/dynamo"
 	"github.com/quintilesims/layer0/common/models"
-	"time"
 )
 
+// todo: this assume entity_id, type, and key are unique
+type DynamoTagSchema struct {
+	EntityType string
+	EntityID   string
+	Tags       map[string]string
+}
+
+func (s DynamoTagSchema) ToTags() models.Tags {
+	tags := models.Tags{}
+	for k, v := range s.Tags {
+		tag := &models.Tag{
+			EntityType: s.EntityType,
+			EntityID:   s.EntityID,
+			Key:        k,
+			Value:      v,
+		}
+
+		tags = append(tags, tag)
+	}
+
+	return tags
+}
+
 type DynamoTagStore struct {
-	table          dynamo.Table
-	consistentRead bool
+	table dynamo.Table
 }
 
 func NewDynamoTagStore(session *session.Session, table string) *DynamoTagStore {
 	db := dynamo.New(session)
 
 	return &DynamoTagStore{
-		table:          db.Table(table),
-		consistentRead: false,
+		table: db.Table(table),
 	}
 }
 
-func (d *DynamoTagStore) setConsistentRead(v bool) {
-	d.consistentRead = v
-}
-
 func (d *DynamoTagStore) Init() error {
-	d.setConsistentRead(true)
 	return nil
 }
 
 func (d *DynamoTagStore) Clear() error {
-	tags, err := d.SelectAll()
-	if err != nil {
-		return err
+	return nil
+}
+
+func (d *DynamoTagStore) Insert(tag models.Tag) error {
+	schema := DynamoTagSchema{
+		EntityType: tag.EntityType,
+		EntityID:   tag.EntityID,
+		Tags:       map[string]string{tag.Key: tag.Value},
 	}
 
-	for _, tag := range tags {
-		if err := d.Delete(tag.TagID); err != nil {
-			return err
+	if err := d.table.Put(schema).If("attribute_not_exists(EntityType)").Run(); err != nil {
+		if err, ok := err.(awserr.Error); ok && err.Code() == "ConditionalCheckFailedException" {
+			return d.insertKey(tag)
 		}
+
+		return err
 	}
 
 	return nil
 }
 
-func (d *DynamoTagStore) Insert(tag *models.Tag) error {
-	// ensure we perform a consistent read after a write
-	defer d.setConsistentRead(true)
-
-	tag.TagID = time.Now().UnixNano()
-	return d.table.Put(tag).Run()
-}
-
-func (d *DynamoTagStore) Delete(tagID int64) error {
-	// ensure we perform a consistent read after a delete
-	defer d.setConsistentRead(true)
-	return d.table.Delete("TagID", tagID).Run()
-}
-
-func (d *DynamoTagStore) SelectAll() (models.Tags, error) {
-	defer d.setConsistentRead(false)
-
-	tags := models.Tags{}
-	if err := d.table.Scan().Consistent(d.consistentRead).All(&tags); err != nil {
-		return nil, err
+func (d *DynamoTagStore) insertKey(tag models.Tag) error {
+	schema, err := d.selectByQuery(tag.EntityType, tag.EntityID)
+	if err != nil {
+		return err
 	}
 
-	return tags, nil
+	schema.Tags[tag.Key] = tag.Value
+	return d.table.Update("EntityType", tag.EntityType).
+		Range("EntityID", tag.EntityID).
+		Set("Tags", schema.Tags).
+		Run()
 }
 
+// todo: don't allow empty args
 func (d *DynamoTagStore) SelectByQuery(entityType, entityID string) (models.Tags, error) {
-	defer d.setConsistentRead(false)
-
-	scan := d.table.Scan()
-
-	switch {
-	case entityType != "" && entityID == "":
-		scan.Filter("'EntityType' = ?", entityType)
-	case entityType == "" && entityID != "":
-		scan.Filter("'EntityID' = ?", entityID)
-	case entityType != "" && entityID != "":
-		scan.Filter("'EntityType' = ? AND $ = ?", entityType, "EntityID", entityID)
-	default:
-		return d.SelectAll()
-	}
-
-	tags := models.Tags{}
-	if err := scan.Consistent(d.consistentRead).All(&tags); err != nil {
+	schema, err := d.selectByQuery(entityType, entityID)
+	if err != nil {
 		return nil, err
 	}
 
+	return schema.ToTags(), nil
+}
+
+func (d *DynamoTagStore) selectByQuery(entityType, entityID string) (*DynamoTagSchema, error) {
+	if entityType == "" {
+		return nil, fmt.Errorf("EntityType is required")
+	}
+
+	if entityID == "" {
+		return nil, fmt.Errorf("EntityID is required")
+	}
+
+	var schema *DynamoTagSchema
+	if err := d.table.Get("EntityType", entityType).
+		Range("EntityID", dynamo.Equal, entityID).
+		Consistent(true).
+		One(&schema); err != nil {
+		return nil, err
+	}
+
+	if schema.Tags == nil {
+		schema.Tags = map[string]string{}
+	}
+
+	return schema, nil
+}
+
+func (d *DynamoTagStore) SelectByType(entityType string) (models.Tags, error) {
+	schemas, err := d.selectByType(entityType)
+	if err != nil {
+		return nil, err
+	}
+
+	tags := models.Tags{}
+	for _, schema := range schemas {
+		tags = append(tags, schema.ToTags()...)
+	}
+
 	return tags, nil
+}
+
+func (d *DynamoTagStore) selectByType(entityType string) ([]*DynamoTagSchema, error) {
+	var schemas []*DynamoTagSchema
+
+	if err := d.table.Get("EntityType", entityType).
+		//Range("EntityID", dynamo.Equal, entityID).
+		Consistent(true).
+		All(&schemas); err != nil {
+		return nil, err
+	}
+
+	return schemas, nil
+}
+
+// todo: we should delete by all tags, or by key
+func (d *DynamoTagStore) Delete(tagID int64) error {
+	return nil
 }
