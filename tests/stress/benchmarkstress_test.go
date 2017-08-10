f6f4ec201d2a793a31f194c937e5636d36902ddb
Tanner Lake <tanner.lake@gmail.com>
2017-08-10 14:00:25 -0700

    Parametrize deploy command

--- tests/stress/benchmarkstress_test.go
+++ tests/stress/benchmarkstress_test.go
@@ -5,22 +5,34 @@ import (
 	"testing"
 )
 
-func BenchmarkStress1Environment0Deploys0Services(b *testing.B)   { benchmarkStress(1, 0, 0, b) }
-func BenchmarkStress5Environments0Deploys0Services(b *testing.B)  { benchmarkStress(5, 0, 0, b) }
-func BenchmarkStress10Environments0Deploys0Services(b *testing.B) { benchmarkStress(10, 0, 0, b) }
+const (
+	serviceCommand = "while true ; do echo LONG RUNNING SERVICE ; sleep 10 ; done"
+	taskCommand    = "sleep 10"
+)
 
-func BenchmarkStress1Environment1Deploy1Service(b *testing.B)   { benchmarkStress(1, 1, 1, b) }
-func BenchmarkStress1Environment1Deploy5Services(b *testing.B)  { benchmarkStress(1, 1, 5, b) }
-func BenchmarkStress1Environment1Deploy10Services(b *testing.B) { benchmarkStress(1, 1, 10, b) }
+func BenchmarkStress1Environment0Deploys0Services(b *testing.B)   { benchmarkStress(1, 0, 0, "", b) }
+func BenchmarkStress5Environments0Deploys0Services(b *testing.B)  { benchmarkStress(5, 0, 0, "", b) }
+func BenchmarkStress10Environments0Deploys0Services(b *testing.B) { benchmarkStress(10, 0, 0, "", b) }
 
-func benchmarkStress(env int, dep int, ser int, b *testing.B) {
+func BenchmarkStress1Environment1Deploy1Service(b *testing.B) {
+	benchmarkStress(1, 1, 1, serviceCommand, b)
+}
+func BenchmarkStress1Environment1Deploy5Services(b *testing.B) {
+	benchmarkStress(1, 1, 5, serviceCommand, b)
+}
+func BenchmarkStress1Environment1Deploy10Services(b *testing.B) {
+	benchmarkStress(1, 1, 10, serviceCommand, b)
+}
+
+func benchmarkStress(env int, dep int, ser int, cmd string, b *testing.B) {
 	tfvars := map[string]string{
 		"num_environments": strconv.Itoa(env),
 		"num_deploys":      strconv.Itoa(dep),
 		"num_services":     strconv.Itoa(ser),
+		"deploy_command":   cmd,
 	}
 
-	log.Debugf("Testing with Environments: %v, Deploys: %v, Services: %v", env, dep, ser)
+	log.Debugf("Testing with Environments: %v, Deploys: %v, Services: %v, Deploy Command: '%v'", env, dep, ser, cmd)
 
 	s := NewStressTest(b, "cases/stress", tfvars)
 	s.Terraform.Apply()
