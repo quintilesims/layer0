87440c976d7ca833d9224b6587912e373f888096
Tanner Lake <tanner.lake@gmail.com>
2017-08-11 11:24:56 -0700

    Increase parametrization/tableization of tests

--- tests/stress/benchmarkstress_test.go
+++ tests/stress/benchmarkstress_test.go
@@ -1,6 +1,7 @@
 package system
 
 import (
+	"fmt"
 	"strconv"
 	"testing"
 )
@@ -10,57 +11,57 @@ const (
 	taskCommand    = "sleep 10"
 )
 
-func BenchmarkStress1Environment0Deploys0Services(b *testing.B) {
-	benchmarkStress(1, 0, 0, "", b)
-}
-func BenchmarkStress10Environments0Deploys0Services(b *testing.B) {
-	benchmarkStress(10, 0, 0, "", b)
-}
-func BenchmarkStress50Environments0Deploys0Services(b *testing.B) {
-	benchmarkStress(50, 0, 0, "", b)
+type BenchmarkTestCase struct {
+	Environments  int
+	Deploys       int
+	Services      int
+	DeployCommand string
 }
 
-func BenchmarkStress1Environment1Deploy1Service(b *testing.B) {
-	benchmarkStress(1, 1, 1, serviceCommand, b)
-}
-func BenchmarkStress1Environment1Deploy5Services(b *testing.B) {
-	benchmarkStress(1, 1, 5, serviceCommand, b)
-}
-func BenchmarkStress1Environment1Deploy10Services(b *testing.B) {
-	benchmarkStress(1, 1, 10, serviceCommand, b)
-}
-
-func benchmarkStress(env int, dep int, ser int, cmd string, b *testing.B) {
-	tfvars := map[string]string{
-		"num_environments": strconv.Itoa(env),
-		"num_deploys":      strconv.Itoa(dep),
-		"num_services":     strconv.Itoa(ser),
-		"deploy_command":   cmd,
+func BenchmarkStress(b *testing.B) {
+	testCases := []BenchmarkTestCase{
+		{1, 0, 0, ""},
+		{10, 0, 0, ""},
+		{50, 0, 0, ""},
+		{1, 1, 1, serviceCommand},
+		{1, 1, 10, serviceCommand},
+		{1, 1, 50, serviceCommand},
 	}
 
-	log.Debugf("Testing with Environments: %v, Deploys: %v, Services: %v, Deploy Command: '%v'", env, dep, ser, cmd)
-
-	s := NewStressTest(b, "cases/stress", tfvars)
-	s.Terraform.Apply()
-	defer s.Terraform.Destroy()
-
-	log.Debug("Benchmarking list operations")
-
-	b.Run("ListEnvironments", func(b *testing.B) {
-		for n := 0; n < b.N; n++ {
-			s.Layer0.ListEnvironments()
+	for _, tc := range testCases {
+		tfvars := map[string]string{
+			"num_environments": strconv.Itoa(tc.Environments),
+			"num_deploys":      strconv.Itoa(tc.Deploys),
+			"num_services":     strconv.Itoa(tc.Services),
+			"deploy_command":   tc.DeployCommand,
 		}
-	})
 
-	b.Run("ListDeploys", func(b *testing.B) {
-		for n := 0; n < b.N; n++ {
-			s.Layer0.ListDeploys()
-		}
-	})
+		log.Debugf("Testing with Environments: %v, Deploys: %v, Services: %v, Deploy Command: '%v'", tc.Environments, tc.Deploys, tc.Services, tc.DeployCommand)
 
-	b.Run("ListServices", func(b *testing.B) {
-		for n := 0; n < b.N; n++ {
-			s.Layer0.ListServices()
-		}
-	})
+		s := NewStressTest(b, "cases/stress", tfvars)
+		s.Terraform.Apply()
+		defer s.Terraform.Destroy()
+
+		log.Debug("Benchmarking list operations")
+
+		title := fmt.Sprintf("Envs:%v_Deps:%v_Svcs:%v/", tc.Environments, tc.Deploys, tc.Services)
+
+		b.Run(title+"ListEnvironments", func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				s.Layer0.ListEnvironments()
+			}
+		})
+
+		b.Run(title+"ListDeploys", func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				s.Layer0.ListDeploys()
+			}
+		})
+
+		b.Run(title+"ListServices", func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				s.Layer0.ListServices()
+			}
+		})
+	}
 }
