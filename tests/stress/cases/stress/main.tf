1cfe0e8b78b688e116593584151fe21d3022d34f
Josh Parsons <jparsons04@gmail.com>
2017-08-11 17:30:14 -0700

    232: Stress tests split into tf modules, folding some changes from 232-tlake
    I created three tf modules, environment, service and deploy. The outputs
    of environment and deploy are picked up by service. Services are distributed
    using element() amongst available environments and deploys.

--- tests/stress/cases/stress/main.tf
+++ tests/stress/cases/stress/main.tf
@@ -4,26 +4,23 @@ provider "layer0" {
   skip_ssl_verify = true
 }
 
-resource "layer0_environment" "te" {
-  name = "te-${var.num_environments}-${count.index}"
-  size = "t2.micro"
+module "environment" {
+  source = "modules/environment"
 
-  count = "${var.num_environments}"
+  num_environments = "${var.num_environments}"
 }
 
-resource "layer0_service" "ts" {
-  name          = "ts-${var.num_services}-${count.index}"
-  environment   = "${layer0_environment.te.id}"
-  deploy        = "${layer0_deploy.td.id}"
-  scale         = 1
-  wait          = true
 
-  count = "${var.num_services}"
+module "deploy" {
+  source = "modules/deploy"
+
+  num_deploys = "${var.num_deploys}"
 }
 
-resource "layer0_deploy" "td" {
-   name    = "td-${var.num_deploys}-${count.index}"
-   content = "${file("Dockerrun.aws.json")}"
-
-   count = "${var.num_deploys}"
+module "service" {
+  source = "modules/service"
+  num_services = "${var.num_services}"
+  environment_ids = "${module.environment.environment_ids}"
+  deploy_ids = "${module.deploy.deploy_ids}"
 }
+
