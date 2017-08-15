264d0e7eed495e67668834ce066892d248c7719c
zpatrick <zpatrick319@hotmail.com>
2017-08-15 23:02:36 +0000

    implement environment delete

--- api/provider/aws/environment_delete.go
+++ api/provider/aws/environment_delete.go
@@ -1,5 +1,111 @@
 package aws
 
+import (
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/autoscaling"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/ecs"
+)
+
 func (e *EnvironmentProvider) Delete(environmentID string) error {
+	//todo: use conifig.Instance()
+	fqEnvironmentID := addLayer0Prefix("INSTANCE", environmentID)
+
+	if err := e.deleteASG(fqEnvironmentID); err != nil {
+		return err
+	}
+
+	if err := e.deleteLC(fqEnvironmentID); err != nil {
+		return err
+	}
+
+	securityGroup, err := e.readSG(fqEnvironmentID)
+	if err != nil && !strings.Contains(err.Error(), "does not exist") {
+		return err
+	}
+
+	if securityGroup != nil {
+		if err := e.deleteSG(aws.StringValue(securityGroup.GroupId)); err != nil {
+			return err
+		}
+	}
+
+	if err := e.deleteCluster(fqEnvironmentID); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (e *EnvironmentProvider) deleteASG(autoScalingGroupName string) error {
+	input := &autoscaling.DeleteAutoScalingGroupInput{}
+	input.SetAutoScalingGroupName(autoScalingGroupName)
+	input.SetForceDelete(true)
+
+	if err := input.Validate(); err != nil {
+		return err
+	}
+
+	if _, err := e.AWS.AutoScaling.DeleteAutoScalingGroup(input); err != nil {
+		if err, ok := err.(awserr.Error); ok && strings.Contains(err.Message(), "AutoScalingGroup name not found") {
+			return nil
+		}
+
+		return err
+	}
+
+	return nil
+}
+
+func (e *EnvironmentProvider) deleteLC(launchConfigName string) error {
+	input := &autoscaling.DeleteLaunchConfigurationInput{}
+	input.SetLaunchConfigurationName(launchConfigName)
+
+	if err := input.Validate(); err != nil {
+		return err
+	}
+
+	if _, err := e.AWS.AutoScaling.DeleteLaunchConfiguration(input); err != nil {
+		if err, ok := err.(awserr.Error); ok && strings.Contains(err.Message(), "Launch configuration name not found") {
+			return nil
+		}
+
+		return err
+	}
+
+	return nil
+}
+
+func (e *EnvironmentProvider) deleteSG(securityGroupID string) error {
+	input := &ec2.DeleteSecurityGroupInput{}
+	input.SetGroupId(securityGroupID)
+
+	// todo: catch does not exist error
+	if _, err := e.AWS.EC2.DeleteSecurityGroup(input); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (e *EnvironmentProvider) deleteCluster(clusterName string) error {
+	input := &ecs.DeleteClusterInput{}
+	input.SetCluster(clusterName)
+
+	if err := input.Validate(); err != nil {
+		return err
+	}
+
+	if _, err := e.AWS.ECS.DeleteCluster(input); err != nil {
+		if err, ok := err.(awserr.Error); ok && err.Code() == "ClusterNotFoundException" {
+			return nil
+		}
+
+		return err
+	}
+
 	return nil
 }
