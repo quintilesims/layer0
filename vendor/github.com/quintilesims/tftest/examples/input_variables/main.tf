82b91d7c9ee1ba86dce931e7713e1c06d4783aba
Josh Parsons <jparsons04@gmail.com>
2017-08-22 13:42:24 -0700

    232: syncing vendor dir state from develop into 232-stress-tests

--- vendor/github.com/quintilesims/tftest/examples/input_variables/main.tf
+++
@@ -1,7 +0,0 @@
-variable "message_one" {}
-
-variable "message_two" {}
-
-output "combined_messages" {
-  value = "${var.message_one} ${var.message_two}"
-}
--- vendor/github.com/quintilesims/tftest/examples/setup_teardown/setup/main.tf
+++
@@ -1,3 +0,0 @@
-variable "message" {
-  default = "Hello, World"
-}
