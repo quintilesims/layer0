37cec856b87c553b977a99ef0d29e3283790325f
Tanner Lake <tanner.lake@gmail.com>
2017-08-28 14:51:29 -0700

    Update to align with changes in `tftest` package (#332)
    * Update to work with changes in tftest
    
    * Remove earlier iteration stress test file
    
    * Use 'testing.B' properly to avoid panic

--- tests/stress/stress_test.go
+++ tests/stress/stress_test.go
@@ -1,24 +1,19 @@
 package system
 
 import (
+	"testing"
+
 	"github.com/quintilesims/layer0/common/config"
 	"github.com/quintilesims/layer0/tests/clients"
 	"github.com/quintilesims/tftest"
 )
-
-type Tester interface {
-	Log(args ...interface{})
-	Logf(format string, args ...interface{})
-	Fatal(args ...interface{})
-	Fatalf(format string, args ...interface{})
-}
 
 type StressTest struct {
 	Terraform *tftest.TestContext
 	Layer0    *clients.Layer0TestClient
 }
 
-func NewStressTest(t Tester, dir string, vars map[string]string) *StressTest {
+func NewStressTest(t *testing.B, dir string, vars map[string]string) *StressTest {
 	if vars == nil {
 		vars = map[string]string{}
 	}
@@ -26,11 +21,13 @@ func NewStressTest(t Tester, dir string, vars map[string]string) *StressTest {
 	vars["endpoint"] = config.APIEndpoint()
 	vars["token"] = config.AuthToken()
 
-	tfContext := tftest.NewTestContext(t,
+	tfContext := tftest.NewTestContext(
+		t,
 		tftest.Dir(dir),
 		tftest.Vars(vars),
 		tftest.DryRun(*dry),
-		tftest.Log(log))
+		tftest.Log(t),
+	)
 
 	layer0 := clients.NewLayer0TestClient(t, vars["endpoint"], vars["token"])
 
