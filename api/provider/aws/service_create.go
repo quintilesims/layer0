7818eed161343dbfaf8655d9f559e1b019d5103e
Tanner Lake <tanner.lake@gmail.com>
2017-09-15 13:20:37 -0700

    Break CRUD actions up into separate files

---
+++ api/provider/aws/service_create.go
@@ -0,0 +1,8 @@
+package aws
+
+import "github.com/quintilesims/layer0/common/models"
+
+func (s *ServiceProvider) Create(req models.CreateServiceRequest) (*models.Service, error) {
+
+	return nil, nil
+}
