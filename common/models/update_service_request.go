9313b792d8098cc4ee6e1df424cb1e1dc49dc896
Tanner Lake <tanner.lake@gmail.com>
2017-09-15 14:54:45 -0700

    Add `UpdateServiceRequest` model

--- common/models/update_service_request.go
+++ common/models/update_service_request.go
@@ -1,5 +1,44 @@
 package models
 
+import (
+	"fmt"
+
+	swagger "github.com/zpatrick/go-plugin-swagger"
+)
+
 type UpdateServiceRequest struct {
-	DeployID string `json:"deploy_id"`
+	ServiceID         string  `json:"service_id"`
+	DeployID          *string `json:"deploy_id"`
+	ServiceScaleCount *int    `json:"service_scale_count"`
+}
+
+func (u UpdateServiceRequest) Validate() error {
+	if u.ServiceID == "" {
+		return fmt.Errorf("ServiceID must be specified")
+	}
+
+	if u.DeployID != nil {
+		if *u.DeployID == "" {
+			return fmt.Errorf("DeployID must be specified")
+		}
+	}
+
+	if u.ServiceScaleCount != nil {
+		if *u.ServiceScaleCount < 0 {
+			return fmt.Errorf("ServiceScaleCount must be a positive integer")
+		}
+	}
+
+	return nil
+}
+
+func (u UpdateServiceRequest) Definition() swagger.Definition {
+	return swagger.Definition{
+		Type: "object",
+		Properties: map[string]swagger.Property{
+			"service_id":          swagger.NewStringProperty(),
+			"deploy_id":           swagger.NewStringProperty(),
+			"service_scale_count": swagger.NewIntProperty(),
+		},
+	}
 }
