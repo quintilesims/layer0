c1f9f3dd88be520edb080ed1d25d83f05b4ad362
Josh Parsons <jparsons04@gmail.com>
2017-09-20 12:49:38 -0700

    274: Task Delete() implementation
    * Rebased work from task-refactor
    
    * Shares helper functions lookupEnvironmentID, deleteEntityTags
    
    * Unit test against deleteEntityTags

--- api/provider/aws/task_delete.go
+++ api/provider/aws/task_delete.go
@@ -1,5 +1,50 @@
 package aws
 
-func (t *TaskProvider) Delete(TaskID string) error {
+import (
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/service/ecs"
+)
+
+func (t *TaskProvider) Delete(taskID string) error {
+	environmentID, err := t.lookupTaskEnvironmentID(taskID)
+	if err != nil {
+		return err
+	}
+	fqEnvironmentID := addLayer0Prefix(t.Config.Instance(), environmentID)
+
+	taskARN, err := t.lookupTaskARN(taskID)
+	if err != nil {
+		return err
+	}
+
+	clusterName := fqEnvironmentID
+	if err := t.stopTask(clusterName, taskARN); err != nil {
+		return err
+	}
+
+	if err := deleteEntityTags(t.TagStore, "task", taskID); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *TaskProvider) stopTask(clusterName, taskARN string) error {
+	input := &ecs.StopTaskInput{}
+	input.SetCluster(clusterName)
+	input.SetTask(taskARN)
+
+	if err := input.Validate(); err != nil {
+		return err
+	}
+
+	if _, err := t.AWS.ECS.StopTask(input); err != nil {
+		if err, ok := err.(awserr.Error); ok && strings.Contains(err.Message(), "task was not found") {
+			return nil
+		}
+	}
+
 	return nil
 }
