3f111d1625fec74098d56782b323b1b3cc3bfac8
Josh Parsons <jparsons04@gmail.com>
2017-09-25 14:55:49 -0700

    274: PR #349, Deploy Read() implemented, unit test, fix to controllers Deploy test

--- api/provider/aws/deploy_read.go
+++ api/provider/aws/deploy_read.go
@@ -1,9 +1,64 @@
 package aws
 
 import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/service/ecs"
 	"github.com/quintilesims/layer0/common/models"
 )
 
 func (d *DeployProvider) Read(deployID string) (*models.Deploy, error) {
-	return nil, nil
+	fqDeployID := addLayer0Prefix(d.Config.Instance(), deployID)
+	familyName := fqDeployID
+
+	taskDefinitionOutput, err := d.describeTaskDefinition(familyName)
+	if err != nil {
+		return nil, err
+	}
+
+	deployFile, err := json.Marshal(taskDefinitionOutput)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to extract deploy file: %s", err.Error())
+	}
+
+	model := &models.Deploy{
+		DeployID:   deployID,
+		DeployFile: deployFile,
+	}
+
+	if err := d.populateModelTags(deployID, model); err != nil {
+		return nil, err
+	}
+
+	return model, nil
+}
+
+func (d *DeployProvider) describeTaskDefinition(familyName string) (*ecs.TaskDefinition, error) {
+	input := &ecs.DescribeTaskDefinitionInput{}
+	input.SetTaskDefinition(familyName)
+
+	output, err := d.AWS.ECS.DescribeTaskDefinition(input)
+	if err != nil {
+		return nil, err
+	}
+
+	return output.TaskDefinition, nil
+}
+
+func (d *DeployProvider) populateModelTags(deployID string, model *models.Deploy) error {
+	tags, err := d.TagStore.SelectByTypeAndID("deploy", deployID)
+	if err != nil {
+		return err
+	}
+
+	if tag, ok := tags.WithKey("name").First(); ok {
+		model.DeployName = tag.Value
+	}
+
+	if tag, ok := tags.WithKey("version").First(); ok {
+		model.Version = tag.Value
+	}
+
+	return nil
 }
