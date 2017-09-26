7dc2ae7cf47e5f0570f5cbfb763714ea52f82b8b
Josh Parsons <jparsons04@gmail.com>
2017-09-26 12:55:42 -0700

    274: Add the implementation of List() for Deploy provider with unit test

--- api/provider/aws/deploy_list.go
+++ api/provider/aws/deploy_list.go
@@ -1,9 +1,69 @@
 package aws
 
 import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ecs"
 	"github.com/quintilesims/layer0/common/models"
 )
 
 func (d *DeployProvider) List() ([]models.DeploySummary, error) {
-	return nil, nil
+	taskDefinitionARNs, err := d.listTaskDefinitionARNs()
+	if err != nil {
+		return nil, err
+	}
+
+	summaries, err := d.populateSummariesFromTaskDefinitionARNs(taskDefinitionARNs)
+	if err != nil {
+		return nil, err
+	}
+
+	return summaries, nil
+}
+
+func (d *DeployProvider) listTaskDefinitionARNs() ([]string, error) {
+	taskDefinitionARNs := []string{}
+	fn := func(output *ecs.ListTaskDefinitionsOutput, lastPage bool) bool {
+		for _, taskDefinitionARN := range output.TaskDefinitionArns {
+			taskDefinitionARNs = append(taskDefinitionARNs, aws.StringValue(taskDefinitionARN))
+		}
+		return !lastPage
+	}
+
+	input := &ecs.ListTaskDefinitionsInput{}
+	if err := d.AWS.ECS.ListTaskDefinitionsPages(input, fn); err != nil {
+		return nil, err
+	}
+
+	return taskDefinitionARNs, nil
+}
+
+func (d *DeployProvider) populateSummariesFromTaskDefinitionARNs(taskDefinitionARNs []string) ([]models.DeploySummary, error) {
+	deployTags, err := d.TagStore.SelectByType("deploy")
+	if err != nil {
+		return nil, err
+	}
+
+	summaries := make([]models.DeploySummary, 0, len(taskDefinitionARNs))
+	for _, taskDefinitionARN := range taskDefinitionARNs {
+		for _, tag := range deployTags.WithKey("arn") {
+			// Populate only if there is a matching ARN in tags db
+			if tag.Value == taskDefinitionARN {
+				summary := models.DeploySummary{
+					DeployID: tag.EntityID,
+				}
+
+				if tag, ok := deployTags.WithID(summary.DeployID).WithKey("name").First(); ok {
+					summary.DeployName = tag.Value
+				}
+
+				if tag, ok := deployTags.WithID(summary.DeployID).WithKey("version").First(); ok {
+					summary.Version = tag.Value
+				}
+
+				summaries = append(summaries, summary)
+			}
+		}
+	}
+
+	return summaries, nil
 }
---
+++ api/provider/aws/deploy_list_test.go
@@ -0,0 +1,93 @@
+package aws
+
+import (
+	"testing"
+
+	"github.com/quintilesims/layer0/api/tag"
+	"github.com/quintilesims/layer0/common/models"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDeploy_populateSummariesFromTaskDefinitionARNs(t *testing.T) {
+	tagStore := tag.NewMemoryStore()
+	deploy := NewDeployProvider(nil, tagStore, nil)
+
+	tags := models.Tags{
+		{
+			EntityID:   "d1",
+			EntityType: "deploy",
+			Key:        "name",
+			Value:      "deploy1",
+		},
+		{
+			EntityID:   "d1",
+			EntityType: "deploy",
+			Key:        "version",
+			Value:      "3",
+		},
+		{
+			EntityID:   "d1",
+			EntityType: "deploy",
+			Key:        "arn",
+			Value:      "arn1",
+		},
+		{
+			EntityID:   "d2",
+			EntityType: "deploy",
+			Key:        "name",
+			Value:      "deploy2",
+		},
+		{
+			EntityID:   "d2",
+			EntityType: "deploy",
+			Key:        "version",
+			Value:      "4",
+		},
+		{
+			EntityID:   "d2",
+			EntityType: "deploy",
+			Key:        "arn",
+			Value:      "arn2",
+		},
+		{
+			EntityID:   "d3",
+			EntityType: "environment",
+			Key:        "name",
+			Value:      "bad_deploy2",
+		},
+		{
+			EntityID:   "d3",
+			EntityType: "deploy",
+			Key:        "version",
+			Value:      "5",
+		},
+		{
+			EntityID:   "d3",
+			EntityType: "deploy",
+			Key:        "arn",
+			Value:      "arn3",
+		},
+	}
+
+	for _, tag := range tags {
+		if err := tagStore.Insert(tag); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	taskDefinitionARNs := []string{
+		"arn1",
+		"arn2",
+	}
+
+	summaries, err := deploy.populateSummariesFromTaskDefinitionARNs(taskDefinitionARNs)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Len(t, summaries, 2)
+	assert.Equal(t, "deploy1", summaries[0].DeployName)
+	assert.Equal(t, "3", summaries[0].Version)
+	assert.Equal(t, "deploy2", summaries[1].DeployName)
+	assert.Equal(t, "4", summaries[1].Version)
+}
