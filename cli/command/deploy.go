ca33837fd89a3bde463c697a38422ce0e4a8e02c
Chemudugunta, Seshi <seshi.chemudugunta@us.imshealth.com>
2017-10-09 18:06:51 -0700

    CommandFactory to CommandMediator

--- cli/command/deploy.go
+++ cli/command/deploy.go
@@ -2,7 +2,16 @@ package command
 
 import "github.com/urfave/cli"
 
-func (f *CommandFactory) Deploy() cli.Command {
+type DeployCommand struct {
+	*CommandMediator
+}
+
+func NewDeploy(m *CommandMediator) cli.Command {
+	// a later reference d - commented out for now to avoid compile-time error
+	// d = &DeployCommand{
+	// 	CommandMediator: m,
+	// }
+
 	return cli.Command{
 		Name:        "deploy",
 		Usage:       "manage layer0 deploys",
--- cli/command/environment.go
+++ cli/command/environment.go
@@ -9,51 +9,59 @@ import (
 	"github.com/urfave/cli"
 )
 
-func (f *CommandFactory) Environment() cli.Command {
+type EnvironmentCommand struct {
+	*CommandMediator
+}
+
+func NewEnvironment(m *CommandMediator) cli.Command {
+	e := &EnvironmentCommand{
+		CommandMediator: m,
+	}
+
 	return cli.Command{
 		Name:  "environment",
 		Usage: "manage layer0 environments",
 		Subcommands: []cli.Command{
-			// {
-			// Name:      "create",
-			// Usage:     "create a new environment",
-			// Action:    f.createEnvironment,
-			// ArgsUsage: "NAME",
-			// Flags: []cli.Flag{
-			// cli.StringFlag{
-			// Name:  "size",
-			// Value: "m3.medium",
-			// Usage: "size of the ec2 instances to use in the environment cluster",
-			// },
-			// cli.IntFlag{
-			// Name:  "min-count",
-			// Value: 0,
-			// Usage: "minimum number of instances allowed in the environment cluster",
-			// },
-			// cli.StringFlag{
-			// Name:  "user-data",
-			// Usage: "path to user data file",
-			// },
-			// cli.StringFlag{
-			// Name:  "os",
-			// Value: "linux",
-			// Usage: "specifies if the environment will run windows or linux containers",
-			// },
-			// cli.StringFlag{
-			// Name:  "ami",
-			// Usage: "specifies a custom AMI ID to use in the environment",
-			// },
-			// cli.BoolFlag{
-			// Name:  "nowait",
-			// Usage: "don't wait for the job to finish",
-			// },
-			// },
-			// },
+			{
+				Name:      "create",
+				Usage:     "create a new environment",
+				Action:    e.createEnvironment,
+				ArgsUsage: "NAME",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:  "size",
+						Value: "m3.medium",
+						Usage: "size of the ec2 instances to use in the environment cluster",
+					},
+					cli.IntFlag{
+						Name:  "min-count",
+						Value: 0,
+						Usage: "minimum number of instances allowed in the environment cluster",
+					},
+					cli.StringFlag{
+						Name:  "user-data",
+						Usage: "path to user data file",
+					},
+					cli.StringFlag{
+						Name:  "os",
+						Value: "linux",
+						Usage: "specifies if the environment will run windows or linux containers",
+					},
+					cli.StringFlag{
+						Name:  "ami",
+						Usage: "specifies a custom AMI ID to use in the environment",
+					},
+					cli.BoolFlag{
+						Name:  "nowait",
+						Usage: "don't wait for the job to finish",
+					},
+				},
+			},
 			{
 				Name:      "delete",
 				Usage:     "delete an environment",
 				ArgsUsage: "NAME",
-				Action:    f.deleteEnvironment,
+				Action:    e.deleteEnvironment,
 				Flags: []cli.Flag{
 					cli.BoolTFlag{
 						Name:  "wait",
@@ -64,20 +72,20 @@ func (f *CommandFactory) Environment() cli.Command {
 			{
 				Name:      "list",
 				Usage:     "list all environments",
-				Action:    f.listEnvironments,
+				Action:    e.listEnvironments,
 				ArgsUsage: " ",
 			},
 			{
 				Name:      "read",
 				Usage:     "describe an environment",
-				Action:    f.readEnvironment,
+				Action:    e.readEnvironment,
 				ArgsUsage: "NAME",
 			},
 		},
 	}
 }
 
-func (f *CommandFactory) createEnvironment(c *cli.Context) error {
+func (e *EnvironmentCommand) createEnvironment(c *cli.Context) error {
 	args, err := extractArgs(c.Args(), "NAME")
 	if err != nil {
 		return err
@@ -102,49 +110,49 @@ func (f *CommandFactory) createEnvironment(c *cli.Context) error {
 		AMIID:            c.String("ami"),
 	}
 
-	jobID, err := f.client.CreateEnvironment(req)
+	jobID, err := e.client.CreateEnvironment(req)
 	if err != nil {
 		return err
 	}
 
 	if c.GlobalBool(config.FLAG_NO_WAIT) {
 		// todo: use common helper
-		f.printer.Printf("Running as job '%s'", jobID)
+		e.printer.Printf("Running as job '%s'", jobID)
 		return nil
 	}
 
-	f.printer.StartSpinner("creating")
-	defer f.printer.StopSpinner()
+	e.printer.StartSpinner("creating")
+	defer e.printer.StopSpinner()
 
-	job, err := client.WaitForJob(f.client, jobID, c.GlobalDuration(config.FLAG_TIMEOUT))
+	job, err := client.WaitForJob(e.client, jobID, c.GlobalDuration(config.FLAG_TIMEOUT))
 	if err != nil {
 		return err
 	}
 
 	environmentID := job.Result
-	environment, err := f.client.ReadEnvironment(environmentID)
+	environment, err := e.client.ReadEnvironment(environmentID)
 	if err != nil {
 		return err
 	}
 
-	return f.printer.PrintEnvironments(environment)
+	return e.printer.PrintEnvironments(environment)
 }
 
-func (f *CommandFactory) deleteEnvironment(c *cli.Context) error {
-	return f.deleteHelper(c, "environment", func(environmentID string) (string, error) {
-		return f.client.DeleteEnvironment(environmentID)
+func (e *EnvironmentCommand) deleteEnvironment(c *cli.Context) error {
+	return e.deleteHelper(c, "environment", func(environmentID string) (string, error) {
+		return e.client.DeleteEnvironment(environmentID)
 	})
 }
 
-func (f *CommandFactory) listEnvironments(c *cli.Context) error {
-	environmentSummaries, err := f.client.ListEnvironments()
+func (e *EnvironmentCommand) listEnvironments(c *cli.Context) error {
+	environmentSummaries, err := e.client.ListEnvironments()
 	if err != nil {
 		return err
 	}
 
-	return f.printer.PrintEnvironmentSummaries(environmentSummaries...)
+	return e.printer.PrintEnvironmentSummaries(environmentSummaries...)
 }
 
-func (f *CommandFactory) readEnvironment(c *cli.Context) error {
+func (e *EnvironmentCommand) readEnvironment(c *cli.Context) error {
 	return nil
 }
