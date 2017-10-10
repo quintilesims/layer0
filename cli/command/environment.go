d7c095bd38d413b6fea48f8f9ffe609efa96032c
Chemudugunta, Seshi <seshi.chemudugunta@us.imshealth.com>
2017-10-09 18:06:51 -0700

    CommandFactory to CommandMediator

--- cli/command/environment.go
+++ cli/command/environment.go
@@ -1,6 +1,13 @@
 package command
 
-import "github.com/urfave/cli"
+import (
+	"io/ioutil"
+
+	"github.com/quintilesims/layer0/client"
+	"github.com/quintilesims/layer0/common/config"
+	"github.com/quintilesims/layer0/common/models"
+	"github.com/urfave/cli"
+)
 
 type EnvironmentCommand struct {
 	*CommandBase
@@ -11,5 +18,141 @@ func NewEnvironmentCommand(b *CommandBase) *EnvironmentCommand {
 }
 
 func (e *EnvironmentCommand) Command() cli.Command {
-	return cli.Command{}
+	return cli.Command{
+		Name:  "environment",
+		Usage: "manage layer0 environments",
+		Subcommands: []cli.Command{
+			{
+				Name:      "create",
+				Usage:     "create a new environment",
+				Action:    e.createEnvironment,
+				ArgsUsage: "NAME",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:  "size",
+						Value: "m3.medium",
+						Usage: "size of the ec2 instances to use in the environment cluster",
+					},
+					cli.IntFlag{
+						Name:  "min-count",
+						Value: 0,
+						Usage: "minimum number of instances allowed in the environment cluster",
+					},
+					cli.StringFlag{
+						Name:  "user-data",
+						Usage: "path to user data file",
+					},
+					cli.StringFlag{
+						Name:  "os",
+						Value: "linux",
+						Usage: "specifies if the environment will run windows or linux containers",
+					},
+					cli.StringFlag{
+						Name:  "ami",
+						Usage: "specifies a custom AMI ID to use in the environment",
+					},
+					cli.BoolFlag{
+						Name:  "nowait",
+						Usage: "don't wait for the job to finish",
+					},
+				},
+			},
+			{
+				Name:      "delete",
+				Usage:     "delete an environment",
+				ArgsUsage: "NAME",
+				Action:    e.deleteEnvironment,
+				Flags: []cli.Flag{
+					cli.BoolTFlag{
+						Name:  "wait",
+						Usage: "wait for the job to complete before returning",
+					},
+				},
+			},
+			{
+				Name:      "list",
+				Usage:     "list all environments",
+				Action:    e.listEnvironments,
+				ArgsUsage: " ",
+			},
+			{
+				Name:      "read",
+				Usage:     "describe an environment",
+				Action:    e.readEnvironment,
+				ArgsUsage: "NAME",
+			},
+		},
+	}
+}
+
+func (e *EnvironmentCommand) createEnvironment(c *cli.Context) error {
+	args, err := extractArgs(c.Args(), "NAME")
+	if err != nil {
+		return err
+	}
+
+	var userData []byte
+	if path := c.String("user-data"); path != "" {
+		content, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		userData = content
+	}
+
+	req := models.CreateEnvironmentRequest{
+		EnvironmentName:  args["NAME"],
+		InstanceSize:     c.String("size"),
+		MinClusterCount:  c.Int("min-count"),
+		UserDataTemplate: userData,
+		OperatingSystem:  c.String("os"),
+		AMIID:            c.String("ami"),
+	}
+
+	jobID, err := e.client.CreateEnvironment(req)
+	if err != nil {
+		return err
+	}
+
+	if c.GlobalBool(config.FLAG_NO_WAIT) {
+		// todo: use common helper
+		e.printer.Printf("Running as job '%s'", jobID)
+		return nil
+	}
+
+	e.printer.StartSpinner("creating")
+	defer e.printer.StopSpinner()
+
+	job, err := client.WaitForJob(e.client, jobID, c.GlobalDuration(config.FLAG_TIMEOUT))
+	if err != nil {
+		return err
+	}
+
+	environmentID := job.Result
+	environment, err := e.client.ReadEnvironment(environmentID)
+	if err != nil {
+		return err
+	}
+
+	return e.printer.PrintEnvironments(environment)
+}
+
+func (e *EnvironmentCommand) deleteEnvironment(c *cli.Context) error {
+	return e.deleteHelper(c, "environment", func(environmentID string) (string, error) {
+		return e.client.DeleteEnvironment(environmentID)
+	})
+}
+
+func (e *EnvironmentCommand) listEnvironments(c *cli.Context) error {
+	environmentSummaries, err := e.client.ListEnvironments()
+	if err != nil {
+		return err
+	}
+
+	return e.printer.PrintEnvironmentSummaries(environmentSummaries...)
+}
+
+func (e *EnvironmentCommand) readEnvironment(c *cli.Context) error {
+	return nil
 }
--- cli/main.go
+++ cli/main.go
@@ -28,6 +28,7 @@ func main() {
 	app.Version = Version
 	app.Flags = config.CLIFlags()
 
+<<<<<<< 058026bb31c76f9af4c89a26f855677f76370c25
 	base := &command.CommandBase{}
 	app.Commands = []cli.Command{
 		command.NewAdminCommand(base).Command(),
@@ -37,6 +38,13 @@ func main() {
 		command.NewLoadBalancerCommand(base).Command(),
 		command.NewServiceCommand(base).Command(),
 		command.NewTaskCommand(base).Command(),
+=======
+	mediator := &command.CommandMediator{}
+	app.Commands = []cli.Command{
+		command.NewDeploy(mediator),
+		command.NewEnvironment(mediator),
+		// todo: other entities
+>>>>>>> CommandFactory to CommandMediator
 	}
 
 	app.Before = func(c *cli.Context) error {
@@ -53,7 +61,15 @@ func main() {
 			VerifySSL: !c.GlobalBool(config.FLAG_SKIP_VERIFY_SSL),
 		})
 
+<<<<<<< 058026bb31c76f9af4c89a26f855677f76370c25
 		tagResolver := resolver.NewTagResolver(apiClient)
+=======
+		mediator.SetClient(apiClient)
+
+		// inject the resolver
+		tagResolver := resolver.NewTagResolver(apiClient)
+		mediator.SetResolver(tagResolver)
+>>>>>>> CommandFactory to CommandMediator
 
 		var p printer.Printer
 		switch format := c.GlobalString(config.FLAG_OUTPUT); format {
@@ -65,9 +81,13 @@ func main() {
 			return fmt.Errorf("Unrecognized output format '%s'", format)
 		}
 
+<<<<<<< 058026bb31c76f9af4c89a26f855677f76370c25
 		base.SetClient(apiClient)
 		base.SetResolver(tagResolver)
 		base.SetPrinter(p)
+=======
+		mediator.SetPrinter(p)
+>>>>>>> CommandFactory to CommandMediator
 
 		// todo: verify version
 
