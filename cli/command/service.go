9e165c2d2f93a7699a108b96f1c5690d7ed82783
Tanner Lake <tanner.lake@gmail.com>
2017-10-10 14:12:18 -0700

    Add service create

--- cli/command/service.go
+++ cli/command/service.go
@@ -1,6 +1,9 @@
 package command
 
-import "github.com/urfave/cli"
+import (
+	"github.com/quintilesims/layer0/common/models"
+	"github.com/urfave/cli"
+)
 
 type ServiceCommand struct {
 	*CommandBase
@@ -10,6 +13,169 @@ func NewServiceCommand(b *CommandBase) *ServiceCommand {
 	return &ServiceCommand{b}
 }
 
-func (e *ServiceCommand) Command() cli.Command {
-	return cli.Command{}
+func (s *ServiceCommand) Command() cli.Command {
+	return cli.Command{
+		Name:  "service",
+		Usage: "manage Layer0 services",
+		Subcommands: []cli.Command{
+			{
+				Name:      "create",
+				Usage:     "create a new service",
+				Action:    s.create,
+				ArgsUsage: "ENVIRONMENT NAME DEPLOY",
+				Flags: []cli.Flag{
+					cli.StringFlag{
+						Name:  "loadbalancer",
+						Usage: "attach the service to the specified load balancer",
+					},
+					cli.BoolFlag{
+						Name:  "nowait",
+						Usage: "don't wait for deployment to complete before returning",
+					},
+				},
+			},
+			{
+				Name:      "delete",
+				Usage:     "delete a service",
+				Action:    s.delete,
+				ArgsUsage: "NAME",
+				Flags: []cli.Flag{
+					cli.BoolFlag{
+						Name:  "nowait",
+						Usage: "don't wait for the job to complete before returning",
+					},
+				},
+			},
+			{
+				Name:   "list",
+				Usage:  "list all services",
+				Action: s.list,
+			},
+			{
+				Name:      "logs",
+				Usage:     "get the logs for a service",
+				Action:    s.logs,
+				ArgsUsage: "NAME",
+				Flags: []cli.Flag{
+					cli.IntFlag{
+						Name:  "tail",
+						Usage: "number of lines from the end to return",
+						Value: 0,
+					},
+					cli.StringFlag{
+						Name:  "start",
+						Usage: "the start of the time range to fetch logs (format: YYYY-MM-DD HH:MM)",
+					},
+					cli.StringFlag{
+						Name:  "end",
+						Usage: "the end of the time range to fetch logs (format: YYYY-MM-DD HH:MM)",
+					},
+				},
+			},
+			{
+				Name:      "read",
+				Usage:     "describe a service",
+				Action:    s.read,
+				ArgsUsage: "NAME",
+			},
+			{
+				Name:      "scale",
+				Usage:     "scale a service",
+				Action:    s.scale,
+				ArgsUsage: "NAME COUNT",
+				Flags: []cli.Flag{
+					cli.BoolFlag{
+						Name:  "nowait",
+						Usage: "don't wait for the job to complete before returning",
+					},
+				},
+			},
+			{
+				Name:      "update",
+				Usage:     "run a new dploy on a service",
+				Action:    s.update,
+				ArgsUsage: "NAME DEPLOY",
+				Flags: []cli.Flag{
+					cli.BoolFlag{
+						Name:  "nowait",
+						Usage: "don't wait for the job to complete before returning",
+					},
+				},
+			},
+		},
+	}
+}
+
+func (s *ServiceCommand) create(c *cli.Context) error {
+	args, err := extractArgs(c.Args(), "ENVIRONMENT", "NAME", "DEPLOY")
+	if err != nil {
+		return err
+	}
+
+	deployID, err := s.resolveSingleEntityIDHelper("deploy", args["DEPLOY"])
+	if err != nil {
+		return err
+	}
+
+	environmentID, err := s.resolveSingleEntityIDHelper("environment", args["ENVIRONMENT"])
+	if err != nil {
+		return err
+	}
+
+	var loadBalancerID string
+	if loadBalancerName := c.String("loadbalancer"); loadBalancerName != "" {
+		id, err := s.resolveSingleEntityIDHelper("load_balancer", loadBalancerName)
+		if err != nil {
+			return err
+		}
+
+		loadBalancerID = id
+	}
+
+	req := models.CreateServiceRequest{
+		DeployID:       deployID,
+		EnvironmentID:  environmentID,
+		LoadBalancerID: loadBalancerID,
+		ServiceName:    args["NAME"],
+	}
+
+	jobID, err := s.client.CreateService(req)
+	if err != nil {
+		return err
+	}
+
+	onCompleteFn := func(serviceID string) error {
+		service, err := s.client.ReadService(serviceID)
+		if err != nil {
+			return err
+		}
+
+		return s.printer.PrintServices(service)
+	}
+
+	return s.waitOnJobHelper(c, jobID, "creating", onCompleteFn)
+}
+
+func (s *ServiceCommand) delete(c *cli.Context) error {
+	return nil
+}
+
+func (s *ServiceCommand) list(c *cli.Context) error {
+	return nil
+}
+
+func (s *ServiceCommand) logs(c *cli.Context) error {
+	return nil
+}
+
+func (s *ServiceCommand) read(c *cli.Context) error {
+	return nil
+}
+
+func (s *ServiceCommand) scale(c *cli.Context) error {
+	return nil
+}
+
+func (s *ServiceCommand) update(c *cli.Context) error {
+	return nil
 }
