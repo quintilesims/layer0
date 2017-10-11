ec2b7b8cc7a7413e2d45a8f03d10e7f06063cfda
Ivers, Brandon (Seattle) <bivers@us.imshealth.com>
2017-10-11 13:36:11 -0700

    add initial admin and deploy cmd impl

--- cli/command/admin.go
+++ cli/command/admin.go
@@ -1,15 +1,69 @@
 package command
 
-import "github.com/urfave/cli"
+import (
+	"github.com/quintilesims/layer0/common/config"
+	"github.com/urfave/cli"
+)
 
 type AdminCommand struct {
 	*CommandBase
 }
 
 func NewAdminCommand(b *CommandBase) *AdminCommand {
-	return &AdminCommand{b}
+	return &AdminCommand{
+		CommandBase: b,
+	}
 }
 
-func (e *AdminCommand) Command() cli.Command {
-	return cli.Command{}
+func (a *AdminCommand) Command() cli.Command {
+	return cli.Command{
+		Name:        "admin",
+		Usage:       "manage the layer0 api",
+		Description: "manage the Layer0 API",
+		Subcommands: []cli.Command{
+			{
+				Name:      "debug",
+				Usage:     "generate debug information",
+				Action:    a.debug,
+				ArgsUsage: " ",
+			},
+		},
+	}
+}
+
+func (a *AdminCommand) debug(c *cli.Context) error {
+	apiEndpoint := c.GlobalString(config.ENVVAR_ENDPOINT)
+	cliAuth := c.GlobalString(config.ENVVAR_TOKEN)
+	cliVersion := c.App.Version
+
+	debugInfo, err := a.client.ReadConfig()
+	if err != nil {
+		return err
+	}
+
+	sslVerify := "enabled"
+	if c.GlobalBool(config.ENVVAR_SKIP_VERIFY_SSL) {
+		sslVerify = "disabled"
+	}
+
+	versionVerify := "enabled"
+	if c.GlobalBool(config.ENVVAR_SKIP_VERIFY_VERSION) {
+		versionVerify = "disabled"
+	}
+
+	a.printer.Printf("DEBUG REPORT \n")
+	a.printer.Printf("------------ \n")
+	a.printer.Printf("Instance Name:    %v\n", debugInfo.Instance)
+	a.printer.Printf("API Endpoint:     %v\n", apiEndpoint)
+	a.printer.Printf("API Version:      %v\n", debugInfo.Version)
+	a.printer.Printf("CLI Version:      %v\n", cliVersion)
+	a.printer.Printf("CLI Auth:         %v\n", cliAuth)
+	a.printer.Printf("SSL Verify:       %v\n", sslVerify)
+	a.printer.Printf("Version Verify:   %v\n", versionVerify)
+	a.printer.Printf("------------ \n")
+	a.printer.Printf("VPC ID:           %v\n", debugInfo.VPCID)
+	a.printer.Printf("Public Subnets:   %v\n", debugInfo.PublicSubnets)
+	a.printer.Printf("Private Subnets:  %v\n", debugInfo.PrivateSubnets)
+
+	return nil
 }
--- cli/command/deploy.go
+++ cli/command/deploy.go
@@ -1,15 +1,161 @@
 package command
 
-import "github.com/urfave/cli"
+import (
+	"github.com/quintilesims/layer0/common/models"
+	"github.com/urfave/cli"
+	"io/ioutil"
+)
 
 type DeployCommand struct {
 	*CommandBase
 }
 
 func NewDeployCommand(b *CommandBase) *DeployCommand {
-	return &DeployCommand{b}
+	return &DeployCommand{
+		CommandBase: b,
+	}
 }
 
 func (d *DeployCommand) Command() cli.Command {
-	return cli.Command{}
+	return cli.Command{
+		Name:  "deploy",
+		Usage: "manage layer0 deploys",
+		Subcommands: []cli.Command{
+			{
+				Name:      "create",
+				Usage:     "create a new deploy",
+				Action:    d.create,
+				ArgsUsage: "PATH NAME",
+			},
+			{
+				Name:      "delete",
+				Usage:     "delete a deploy",
+				ArgsUsage: "NAME",
+				Action:    d.delete,
+			},
+			{
+				Name:      "get",
+				Usage:     "describe a deploy",
+				Action:    d.get,
+				ArgsUsage: "NAME",
+			},
+			{
+				Name:      "list",
+				Usage:     "list all deploys (only the latest versions of each family will be shown)",
+				Action:    d.list,
+				ArgsUsage: " ",
+				Flags: []cli.Flag{
+					cli.BoolFlag{
+						Name:  "all",
+						Usage: "list all versions of all deploys",
+					},
+				},
+			},
+		},
+	}
+}
+
+func (d *DeployCommand) create(c *cli.Context) error {
+	args, err := extractArgs(c.Args(), "PATH", "NAME")
+	if err != nil {
+		return err
+	}
+
+	content, err := ioutil.ReadFile(args["PATH"])
+	if err != nil {
+		return err
+	}
+
+	req := models.CreateDeployRequest{
+		DeployName: args["NAME"],
+		DeployFile: content,
+	}
+
+	jobID, err := d.client.CreateDeploy(req)
+	if err != nil {
+		return err
+	}
+
+	// return d.Printer.PrintDeploys(deploy)
+	return d.waitOnJobHelper(c, jobID, "creating", func(deployID string) error {
+		environment, err := d.client.ReadDeploy(deployID)
+		if err != nil {
+			return err
+		}
+
+		return d.printer.PrintEnvironments(environment)
+	})
+}
+
+func (d *DeployCommand) delete(c *cli.Context) error {
+	return d.delete(c, "deploy", d.Client.DeleteDeploy)
+}
+
+func (d *DeployCommand) get(c *cli.Context) error {
+	deploys := []*models.Deploy{}
+	getDeployf := func(id string) error {
+		deploy, err := d.client.GetDeploy(id)
+		if err != nil {
+			return err
+		}
+
+		deploys = append(deploys, deploy)
+		return nil
+	}
+
+	if err := d.get(c, "deploy", getDeployf); err != nil {
+		return err
+	}
+
+	return d.Printer.PrintDeploys(deploys...)
+}
+
+func (d *DeployCommand) list(c *cli.Context) error {
+	deploySummaries, err := d.Client.ListDeploys()
+	if err != nil {
+		return err
+	}
+
+	if !c.Bool("all") {
+		deploySummaries, err = filterDeploySummaries(deploySummaries)
+		if err != nil {
+			return err
+		}
+	}
+
+	return d.Printer.PrintDeploySummaries(deploySummaries...)
+}
+
+func filterDeploySummaries(deploys []*models.DeploySummary) ([]*models.DeploySummary, error) {
+	catalog := map[string]*models.DeploySummary{}
+
+	for _, deploy := range deploys {
+		if name := deploy.DeployName; name != "" {
+			if _, exists := catalog[name]; !exists {
+				catalog[name] = deploy
+				continue
+			}
+
+			max, err := strconv.Atoi(catalog[name].Version)
+			if err != nil {
+				return nil, err
+			}
+
+			current, err := strconv.Atoi(deploy.Version)
+			if err != nil {
+				return nil, err
+			}
+
+			if current > max {
+				catalog[name] = deploy
+			}
+		}
+	}
+
+	filtered := []*models.DeploySummary{}
+	for _, deploy := range catalog {
+		filtered = append(filtered, deploy)
+	}
+
+	return filtered, nil
 }
