adf7665c3d6b9ecfd9a0147069961d8521c1022d
zpatrick <zpatrick319@hotmail.com>
2017-10-12 19:00:46 +0000

    remove debug code

--- cli/command/environment.go
+++ cli/command/environment.go
@@ -1,8 +1,6 @@
 package command
 
 import (
-	"fmt"
-
 	"github.com/urfave/cli"
 )
 
@@ -15,16 +13,5 @@ func NewEnvironmentCommand(b *CommandBase) *EnvironmentCommand {
 }
 
 func (e *EnvironmentCommand) Command() cli.Command {
-	return cli.Command{
-		Name: "environment",
-		Action: func(c *cli.Context) error {
-			id, err := e.resolveSingleEntityIDHelper(c.Args().Get(0), c.Args().Get(1))
-			if err != nil {
-				return err
-			}
-
-			fmt.Printf("Resolved: '%s'\n", id)
-			return nil
-		},
-	}
+	return cli.Command{}
 }
