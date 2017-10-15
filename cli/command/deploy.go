f26b11d80ae6e0fe54c280d8eaaa4a16f74fa37f
Chemudugunta, Seshi <seshi.chemudugunta@us.imshealth.com>
2017-10-15 16:57:31 -0700

    Added enviornment cli command implementation.

--- cli/command/deploy.go
+++ cli/command/deploy.go
@@ -2,10 +2,11 @@ package command
 
 import (
 	"fmt"
-	"github.com/quintilesims/layer0/common/models"
-	"github.com/urfave/cli"
 	"io/ioutil"
 	"strconv"
+
+	"github.com/quintilesims/layer0/common/models"
+	"github.com/urfave/cli"
 )
 
 type DeployCommand struct {
