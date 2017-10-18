f1914f294df096b09d048d50e4455f21481e3cf7
Tanner Lake <tanner.lake@gmail.com>
2017-10-18 13:18:46 -0700

    Add 'WaitForDeployment()' func

--- cli/command/utils.go
+++ cli/command/utils.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"net/url"
 	"strconv"
+	"time"
 
 	"github.com/quintilesims/layer0/client"
+	"github.com/quintilesims/layer0/common/models"
 )
 
 func extractArgs(received []string, names ...string) (map[string]string, error) {
@@ -37,4 +39,43 @@ func buildLogQueryHelper(start, end string, tail int) url.Values {
 	}
 
 	return query
+}
+
+func WaitForDeployment(client client.Client, serviceID string, timeout time.Duration) (*models.Service, error) {
+	successCount := 0
+	requiredSuccessCount := 3
+
+	check := func() (bool, error) {
+		service, err := client.ReadService(serviceID)
+		if err != nil {
+			return false, err
+		}
+
+		for _, deployment := range service.Deployments {
+			if deployment.DesiredCount != deployment.RunningCount {
+				return false, nil
+			}
+		}
+
+		successCount++
+		return successCount >= requiredSuccessCount, nil
+	}
+
+	delay := time.Second * 5
+	start := time.Now()
+
+	for elapsed := time.Since(start); elapsed <= timeout; time.Sleep(delay) {
+		finished, err := check()
+		if err != nil {
+			return nil, err
+		}
+
+		if finished {
+			return client.ReadService(serviceID)
+		}
+
+		elapsed = time.Since(start)
+	}
+
+	return nil, fmt.Errorf("Deployment of service '%s' has not completed within the timeout '%v'", serviceID, timeout)
 }
