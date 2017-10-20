a4400fcfa425d9e196901edae3ced4ef8413672a
Chemudugunta, Seshi <seshi.chemudugunta@us.imshealth.com>
2017-10-19 17:57:34 -0700

    pr feedback.

--- cli/command/environment.go
+++ cli/command/environment.go
@@ -232,6 +232,7 @@ func (e *EnvironmentCommand) link(c *cli.Context) error {
 		SourceEnvironmentID: sourceEnvironmentID,
 		DestEnvironmentID:   destEnvironmentID,
 	}
+
 	if err := e.client.CreateLink(req); err != nil {
 		return err
 	}
@@ -265,6 +266,7 @@ func (e *EnvironmentCommand) unlink(c *cli.Context) error {
 		SourceEnvironmentID: sourceEnvironmentID,
 		DestEnvironmentID:   destEnvironmentID,
 	}
+
 	if err := e.client.DeleteLink(req); err != nil {
 		return err
 	}
