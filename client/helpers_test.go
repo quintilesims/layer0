f8eec946bce2124b16fca3c623884f855e85c1db
Tanner Lake <tanner.lake@gmail.com>
2017-10-20 14:58:06 -0700

    Use 'gomock.InOrder()' to simulate different behaviors of service deployment.

--- client/helpers_test.go
+++ client/helpers_test.go
@@ -12,37 +12,59 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestWaitForDeployment(t *testing.T) {
+func TestWaitForDeployment_standard(t *testing.T) {
 	defer SetTimeMultiplier(0)()
 
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 	client := mock_client.NewMockClient(ctrl)
 
-	deployments := []models.Deployment{
-		{
-			DesiredCount: 1,
-			RunningCount: 1,
+	pending := &models.Service{
+		Deployments: []models.Deployment{
+			{
+				DesiredCount: 1,
+				RunningCount: 0,
+			},
 		},
 	}
 
-	expected := &models.Service{
-		Deployments:  deployments,
-		DesiredCount: 1,
-		RunningCount: 1,
+	finished := &models.Service{
+		Deployments: []models.Deployment{
+			{
+				DesiredCount: 1,
+				RunningCount: 1,
+			},
+		},
 	}
 
-	client.EXPECT().
-		ReadService("svc_id").
-		Return(expected, nil).
-		AnyTimes()
+	gomock.InOrder(
+		// simulate usual lengthy run
+		client.EXPECT().
+			ReadService("svc_id").
+			Return(pending, nil).
+			Times(3),
+		// simulate flapping service
+		client.EXPECT().
+			ReadService("svc_id").
+			Return(finished, nil).
+			Times(2),
+		client.EXPECT().
+			ReadService("svc_id").
+			Return(pending, nil).
+			Times(2),
+		// simulate finished deployment
+		client.EXPECT().
+			ReadService("svc_id").
+			Return(finished, nil).
+			AnyTimes(),
+	)
 
 	result, err := WaitForDeployment(client, "svc_id", time.Second)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	assert.Equal(t, expected, result)
+	assert.Equal(t, finished, result)
 }
 
 func TestWaitForDeployment_error(t *testing.T) {
@@ -83,8 +105,8 @@ func TestWaitForDeployment_timeout(t *testing.T) {
 
 	client.EXPECT().
 		ReadService("svc_id").
-		AnyTimes().
-		Return(expected, nil)
+		Return(expected, nil).
+		AnyTimes()
 
 	if _, err := WaitForDeployment(client, "svc_id", time.Second); err == nil {
 		t.Fatal("Error was nil!")
