a9e79b06b10ce064629be38316d7f54c125e01d4
Josh Parsons <jparsons04@gmail.com>
2017-10-27 15:03:09 -0700

    MAINT: Changing location of aws tests

--- api/provider/aws/aws_test/load_balancer_create_test.go
+++ api/provider/aws/aws_test/load_balancer_create_test.go
@@ -5,6 +5,7 @@ import (
 
 	aws "github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/aws/aws-sdk-go/service/elb"
 	"github.com/aws/aws-sdk-go/service/iam"
 	"github.com/golang/mock/gomock"
 	. "github.com/quintilesims/layer0/api/provider/aws"
@@ -23,6 +24,7 @@ func TestLoadBalancer_Create(t *testing.T) {
 
 	lbp := mock_provider.NewMockLoadBalancerProvider(ctrl)
 
+	// Read the env security group
 	environmentSGName := "env_id-env"
 	filter := &ec2.Filter{}
 	filter.SetName("group-name")
@@ -31,11 +33,19 @@ func TestLoadBalancer_Create(t *testing.T) {
 	describeEnvSGInput := &ec2.DescribeSecurityGroupsInput{}
 	describeEnvSGInput.SetFilters([]*ec2.Filter{filter})
 
-	environmentSG := &ec2.DescribeSecurityGroupsOutput{}
+	securityGroups := make([]*ec2.SecurityGroup, 1)
+	securityGroup := &ec2.SecurityGroup{}
+	securityGroup.SetGroupName(environmentSGName)
+	securityGroups[0] = securityGroup
+	environmentSG := &ec2.DescribeSecurityGroupsOutput{
+		SecurityGroups: securityGroups,
+	}
+
 	client.EC2.EXPECT().
 		DescribeSecurityGroups(describeEnvSGInput).
 		Return(environmentSG, nil)
 
+	// Create LB security group
 	loadBalancerSGName := "lb_id-lb"
 	createLBSGInput := &ec2.CreateSecurityGroupInput{}
 	createLBSGInput.SetGroupName(loadBalancerSGName)
@@ -49,33 +59,37 @@ func TestLoadBalancer_Create(t *testing.T) {
 	filter.SetName("group-name")
 	filter.SetValues([]*string{aws.String(loadBalancerSGName)})
 
+	// Read the LB security group
 	describeLBSGInput := &ec2.DescribeSecurityGroupsInput{}
 	describeLBSGInput.SetFilters([]*ec2.Filter{filter})
 
-	loadBalancerSG := &ec2.SecurityGroup{}
+	loadBalancerSG := &ec2.DescribeSecurityGroupsOutput{}
 	client.EC2.EXPECT().
 		DescribeSecurityGroups(describeLBSGInput).
 		Return(loadBalancerSG, nil)
 
+	// Authorize SG as ingress to the LB
 	AuthorizeSGIngressInput := &ec2.AuthorizeSecurityGroupIngressInput{}
 	AuthorizeSGIngressInput.SetGroupId("lb_id")
 	AuthorizeSGIngressInput.SetCidrIp("0.0.0.0/0")
-	AuthorizeSGIngressInput.SetIpProtocol("TCP")
-	AuthorizeSGIngressInput.SetFromPort(80)
-	AuthorizeSGIngressInput.SetToPort(80)
+	AuthorizeSGIngressInput.SetIpProtocol("HTTPS")
+	AuthorizeSGIngressInput.SetFromPort(443)
+	AuthorizeSGIngressInput.SetToPort(443)
 
 	client.EC2.EXPECT().
 		AuthorizeSecurityGroupIngress(AuthorizeSGIngressInput)
 
+	// Create IAM role
 	CreateRoleInput := &iam.CreateRoleInput{}
 	CreateRoleInput.SetRoleName("lb_id-lb")
 	CreateRoleInput.SetAssumeRolePolicyDocument(DEFAULT_ASSUME_ROLE_POLICY)
 
-	lbIAMRole := iam.CreateRoleOutput{}
+	lbIAMRole := &iam.CreateRoleOutput{}
 	client.IAM.EXPECT().
 		CreateRole(CreateRoleInput).
 		Return(lbIAMRole, nil)
 
+	// Add inline policy to IAM role
 	putRolePolicyInput := &iam.PutRolePolicyInput{}
 	putRolePolicyInput.SetPolicyName("lb_id-lb")
 	putRolePolicyInput.SetRoleName("lb_id-lb")
@@ -84,13 +98,63 @@ func TestLoadBalancer_Create(t *testing.T) {
 	client.IAM.EXPECT().
 		PutRolePolicy(putRolePolicyInput)
 
+	ports := []models.Port{
+		models.Port{
+			CertificateName: "cert",
+			ContainerPort:   443,
+			HostPort:        443,
+			Protocol:        "https",
+		},
+	}
+	certARNs := &iam.ListServerCertificatesOutput{}
+	// Ports to listeners
+	for _, port := range ports {
+		listener := &elb.Listener{}
+		listener.SetProtocol(port.Protocol)
+		listener.SetLoadBalancerPort(port.HostPort)
+		listener.SetInstancePort(port.ContainerPort)
+
+		if port.CertificateName != "" {
+			// If SSL Cert, lookup cert ARN
+			client.IAM.EXPECT().
+				ListServerCertificates(&iam.ListServerCertificatesInput{}).
+				Return(certARNs, nil)
+
+			listener.SetSSLCertificateId("arn")
+		}
+	}
+
+	// Create load balancer request
+	createLoadBalancerInput := &elb.CreateLoadBalancerInput{}
+	createLoadBalancerInput.SetLoadBalancerName("lb_name")
+	createLoadBalancerInput.SetScheme("internet-facing")
+	createLoadBalancerInput.SetSecurityGroups([]*string{})
+	createLoadBalancerInput.SetSubnets([]*string{})
+	createLoadBalancerInput.SetListeners([]*elb.Listener{})
+
+	client.ELB.EXPECT().
+		CreateLoadBalancer(createLoadBalancerInput)
+
+	healthCheck := &elb.HealthCheck{}
+	// Update health check
+	input := &elb.ConfigureHealthCheckInput{}
+	input.SetLoadBalancerName("lb_name")
+	input.SetHealthCheck(healthCheck)
+
+	client.ELB.EXPECT().
+		ConfigureHealthCheck(input)
+
 	req := models.CreateLoadBalancerRequest{
 		LoadBalancerName: "lb_name",
 		EnvironmentID:    "env_id",
 		IsPublic:         true,
-		Ports:            []models.Port{},
+		Ports:            ports,
 		HealthCheck:      models.HealthCheck{},
 	}
+
+	lbp.EXPECT().
+		Create(req).
+		Return("lb_id", nil)
 
 	if _, err := lbp.Create(req); err != nil {
 		t.Fatal(err)
