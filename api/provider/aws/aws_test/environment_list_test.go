eb65220ff88fb75c45627f5a16f2899a28b4cc9b
zpatrick <zpatrick319@hotmail.com>
2017-10-27 13:13:30 -0700

    fix mocks

--- api/provider/aws/aws_test/environment_list_test.go
+++ api/provider/aws/aws_test/environment_list_test.go
@@ -4,13 +4,13 @@ import (
 	"testing"
 
 	"github.com/golang/mock/gomock"
-	"github.com/quintilesims/layer0/common/aws/mock_aws"
+	"github.com/quintilesims/layer0/common/aws"
 )
 
 func TestEnvironmentList(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
-	mockClient := mock_aws.NewMockClient(ctrl)
+	mockClient := aws.NewMockClient(ctrl)
 	print(mockClient)
 }
