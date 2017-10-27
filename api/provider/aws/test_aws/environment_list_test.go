e6a7c03a98f5b97bb604267e89b8cde0eee9fee4
zpatrick <zpatrick319@hotmail.com>
2017-10-27 16:26:06 -0700

    start on env list and read

---
+++ api/provider/aws/test_aws/environment_list_test.go
@@ -0,0 +1,102 @@
+package test_aws
+
+import (
+	"testing"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ecs"
+	"github.com/golang/mock/gomock"
+	provider "github.com/quintilesims/layer0/api/provider/aws"
+	"github.com/quintilesims/layer0/api/tag"
+	awsc "github.com/quintilesims/layer0/common/aws"
+	"github.com/quintilesims/layer0/common/config/mock_config"
+	"github.com/quintilesims/layer0/common/models"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEnvironmentList(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockAWS := awsc.NewMockClient(ctrl)
+	tagStore := tag.NewMemoryStore()
+	mockConfig := mock_config.NewMockAPIConfig(ctrl)
+
+	// todo: setup helper for config
+	mockConfig.EXPECT().Instance().Return("test").AnyTimes()
+
+	tags := models.Tags{
+		{
+			EntityID:   "env_id1",
+			EntityType: "environment",
+			Key:        "name",
+			Value:      "env_name1",
+		},
+		{
+			EntityID:   "env_id1",
+			EntityType: "environment",
+			Key:        "os",
+			Value:      "os1",
+		},
+		{
+			EntityID:   "env_id2",
+			EntityType: "environment",
+			Key:        "name",
+			Value:      "env_name2",
+		},
+		{
+			EntityID:   "env_id2",
+			EntityType: "environment",
+			Key:        "os",
+			Value:      "os2",
+		},
+	}
+
+	for _, tag := range tags {
+		if err := tagStore.Insert(tag); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// ensure we filter out clusters that don't belong to our instance
+	listClusterPagesFN := func(input *ecs.ListClustersInput, fn func(output *ecs.ListClustersOutput, lastPage bool) bool) error {
+		clusterARNs := []*string{
+			aws.String("arn:aws:ecs:region:012345678910:cluster/l0-test-env_id1"),
+			aws.String("arn:aws:ecs:region:012345678910:cluster/l0-test-env_id2"),
+			aws.String("arn:aws:ecs:region:012345678910:cluster/l0-bad-env_id1"),
+			aws.String("arn:aws:ecs:region:012345678910:cluster/bad2"),
+		}
+
+		output := &ecs.ListClustersOutput{}
+		output.SetClusterArns(clusterARNs)
+		fn(output, true)
+
+		return nil
+	}
+
+	mockAWS.ECS.EXPECT().
+		ListClustersPages(gomock.Any(), gomock.Any()).
+		Do(listClusterPagesFN).
+		Return(nil)
+
+	target := provider.NewEnvironmentProvider(mockAWS.Client(), tagStore, mockConfig)
+	result, err := target.List()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := []models.EnvironmentSummary{
+		{
+			EnvironmentID:   "env_id1",
+			EnvironmentName: "env_name1",
+			OperatingSystem: "os1",
+		},
+		{
+			EnvironmentID:   "env_id2",
+			EnvironmentName: "env_name2",
+			OperatingSystem: "os2",
+		},
+	}
+
+	assert.Equal(t, expected, result)
+}
---
+++ api/provider/aws/test_aws/helpers.go
@@ -0,0 +1,28 @@
+package test_aws
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	awsc "github.com/quintilesims/layer0/common/aws"
+)
+
+func describeSecurityGroupHelper(mockAWS *awsc.MockClient, securityGroupName, securityGroupID string) {
+	filter := &ec2.Filter{}
+	filter.SetName("group-name")
+	filter.SetValues([]*string{aws.String(securityGroupName)})
+
+	input := &ec2.DescribeSecurityGroupsInput{}
+	input.SetFilters([]*ec2.Filter{filter})
+
+	securityGroup := &ec2.SecurityGroup{}
+	securityGroup.SetGroupName(securityGroupName)
+	securityGroup.SetGroupId(securityGroupID)
+
+	output := &ec2.DescribeSecurityGroupsOutput{}
+	output.SetSecurityGroups([]*ec2.SecurityGroup{securityGroup})
+
+	// todo: check input
+	mockAWS.EC2.EXPECT().
+		DescribeSecurityGroups(input).
+		Return(output, nil)
+}
