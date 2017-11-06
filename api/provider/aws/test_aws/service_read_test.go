72bb9a07a142beaf94ca1157d628c731355a020f
Tanner Lake <tanner.lake@gmail.com>
2017-11-06 13:50:21 -0800

    Add service provider read: service not found exception test

--- api/provider/aws/test_aws/service_read_test.go
+++ api/provider/aws/test_aws/service_read_test.go
@@ -5,12 +5,14 @@ import (
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/ecs"
 	"github.com/golang/mock/gomock"
 	provider "github.com/quintilesims/layer0/api/provider/aws"
 	"github.com/quintilesims/layer0/api/tag"
 	awsc "github.com/quintilesims/layer0/common/aws"
 	"github.com/quintilesims/layer0/common/config/mock_config"
+	"github.com/quintilesims/layer0/common/errors"
 	"github.com/quintilesims/layer0/common/models"
 	"github.com/stretchr/testify/assert"
 )
@@ -155,4 +157,100 @@ func TestServiceRead(t *testing.T) {
 	}
 
 	assert.Equal(t, expected, result)
+}
+
+func TestServiceRead_serviceNotFoundError(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockAWS := awsc.NewMockClient(ctrl)
+	tagStore := tag.NewMemoryStore()
+	mockConfig := mock_config.NewMockAPIConfig(ctrl)
+
+	mockConfig.EXPECT().Instance().Return("test").AnyTimes()
+
+	tags := models.Tags{
+		{
+			EntityID:   "env_id",
+			EntityType: "environment",
+			Key:        "name",
+			Value:      "env_name",
+		},
+		{
+			EntityID:   "svc_id",
+			EntityType: "service",
+			Key:        "name",
+			Value:      "svc_name",
+		},
+		{
+			EntityID:   "svc_id",
+			EntityType: "service",
+			Key:        "environment_id",
+			Value:      "env_id",
+		},
+		{
+			EntityID:   "dpl_id",
+			EntityType: "deploy",
+			Key:        "arn",
+			Value:      "arn:aws:ecs:region:012345678910:task-definition/dpl_id:1",
+		},
+		{
+			EntityID:   "dpl_id",
+			EntityType: "deploy",
+			Key:        "name",
+			Value:      "dpl_name",
+		},
+		{
+			EntityID:   "dpl_id",
+			EntityType: "deploy",
+			Key:        "version",
+			Value:      "1",
+		},
+	}
+
+	for _, tag := range tags {
+		if err := tagStore.Insert(tag); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	describeServicesInput := &ecs.DescribeServicesInput{}
+	describeServicesInput.SetCluster("l0-test-env_id")
+	describeServicesInput.SetServices([]*string{aws.String("l0-test-svc_id")})
+
+	deployment := &ecs.Deployment{}
+	deployment.SetCreatedAt(time.Time{})
+	deployment.SetDesiredCount(int64(1))
+	deployment.SetPendingCount(int64(0))
+	deployment.SetRunningCount(int64(1))
+	deployment.SetStatus(ecs.DesiredStatusRunning)
+	deployment.SetTaskDefinition("arn:aws:ecs:region:012345678910:task-definition/dpl_id:1")
+	deployment.SetUpdatedAt(time.Time{})
+	deployments := []*ecs.Deployment{
+		deployment,
+	}
+
+	service := &ecs.Service{}
+	service.SetDeployments(deployments)
+	service.SetDesiredCount(int64(1))
+	service.SetPendingCount(int64(0))
+	service.SetRunningCount(int64(1))
+	services := []*ecs.Service{
+		service,
+	}
+
+	describeServicesOutput := &ecs.DescribeServicesOutput{}
+	describeServicesOutput.SetServices(services)
+
+	awsError := awserr.New("ServiceNotFoundException", "", nil)
+
+	mockAWS.ECS.EXPECT().
+		DescribeServices(describeServicesInput).
+		Return(&ecs.DescribeServicesOutput{}, awsError)
+
+	target := provider.NewServiceProvider(mockAWS.Client(), tagStore, mockConfig)
+	_, err := target.Read("svc_id")
+
+	expectedErr := errors.Newf(errors.ServiceDoesNotExist, "Service 'l0-test-svc_id' does not exist")
+	assert.Equal(t, expectedErr, err)
 }
