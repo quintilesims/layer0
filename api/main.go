5b80d0a26f068c7c8dfc1cb46e67080833d5efd3
Tanner Lake <tanner.lake@gmail.com>
2017-12-05 12:30:24 -0800

    pass in jobStore

--- api/main.go
+++ api/main.go
@@ -77,7 +77,7 @@ func main() {
 		serviceProvider := aws.NewServiceProvider(client, tagStore, cfg)
 		taskProvider := aws.NewTaskProvider(client, tagStore, cfg)
 
-		environmentScaler := aws.NewEnvironmentScaler(client, environmentProvider, serviceProvider, taskProvider, cfg)
+		environmentScaler := aws.NewEnvironmentScaler(client, environmentProvider, serviceProvider, taskProvider, jobStore, cfg)
 		scalerDispatcher := scaler.NewDispatcher(environmentProvider, environmentScaler)
 
 		jobRunner := aws.NewJobRunner(
