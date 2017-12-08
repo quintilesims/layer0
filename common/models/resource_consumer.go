eedea7ae4883843481d58f92b9188e1bf909ead7
Tanner Lake <tanner.lake@gmail.com>
2017-12-07 16:31:08 -0800

    Update to use bytesize.Bytesize instead of string

--- common/models/resource_consumer.go
+++ common/models/resource_consumer.go
@@ -1,8 +1,10 @@
 package models
 
+import bytesize "github.com/zpatrick/go-bytesize"
+
 type ResourceConsumer struct {
-	CPU    string `json:"cpu"`
-	ID     string `json:"id"`
-	Memory string `json:"memory"`
-	Ports  []int  `json:"ports"`
+	CPU    bytesize.Bytesize `json:"cpu"`
+	ID     string            `json:"id"`
+	Memory bytesize.Bytesize `json:"memory"`
+	Ports  []int             `json:"ports"`
 }
