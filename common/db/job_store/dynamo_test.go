27bb930aff78b9d3ae8a20f8b0b43bf3626a0d49
Josh Parsons <jparsons@us.imshealth.com>
2018-01-03 10:14:17 -0800

    MAINT: Undo changes to dynamo db tests

--- common/db/job_store/dynamo_test.go
+++ common/db/job_store/dynamo_test.go
@@ -3,11 +3,9 @@ package job_store
 import (
 	"reflect"
 	"testing"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/quintilesims/layer0/common/config"
 	"github.com/quintilesims/layer0/common/models"
@@ -26,17 +24,7 @@ func NewTestJobStore(t *testing.T) *DynamoJobStore {
 		Region:      aws.String(config.AWSRegion()),
 	}
 
-	delay, err := time.ParseDuration(config.AWSTimeBetweenRequests())
-	if err != nil {
-		t.Fatalf("Error parsing time between requests: %v", err)
-	}
-
 	session := session.New(awsConfig)
-	ticker := time.Tick(delay)
-	session.Handlers.Send.PushBack(func(r *request.Request) {
-		<-ticker
-	})
-
 	store := NewDynamoJobStore(session, table)
 
 	if err := store.Clear(); err != nil {
--- common/db/tag_store/dynamo_test.go
+++ common/db/tag_store/dynamo_test.go
@@ -2,11 +2,9 @@ package tag_store
 
 import (
 	"testing"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/quintilesims/layer0/common/config"
 	"github.com/quintilesims/layer0/common/models"
@@ -50,17 +48,7 @@ func NewTestTagStore(t *testing.T) *DynamoTagStore {
 		Region:      aws.String(config.AWSRegion()),
 	}
 
-	delay, err := time.ParseDuration(config.AWSTimeBetweenRequests())
-	if err != nil {
-		t.Fatalf("Error parsing time between requests: %v", err)
-	}
-
 	session := session.New(awsConfig)
-	ticker := time.Tick(delay)
-	session.Handlers.Send.PushBack(func(r *request.Request) {
-		<-ticker
-	})
-
 	store := NewDynamoTagStore(session, table)
 
 	if err := store.Clear(); err != nil {
