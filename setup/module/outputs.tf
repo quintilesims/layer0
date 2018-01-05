0bc44c7e49db32fce37e8235a785f52787031906
Josh Parsons <jparsons@us.imshealth.com>
2018-01-05 12:11:41 -0800

    475: Inclue region as l0-setup endpoint -d output
    The AWS Region specified when creating a layer0 instance is now output
    when running the command l0-setup endpoint -d.
    
    Fixes an AMI ID for the API module in the us-east-1 region

--- setup/module/outputs.tf
+++ setup/module/outputs.tf
@@ -59,9 +59,13 @@ output "windows_service_ami" {
 }
 
 output "dynamo_tag_table" {
-    value = "${module.api.dynamo_tag_table}"
+  value = "${module.api.dynamo_tag_table}"
 }
 
 output "dynamo_job_table" {
-    value = "${module.api.dynamo_job_table}"
+  value = "${module.api.dynamo_job_table}"
+}
+
+output "region" {
+  value = "${var.region}"
 }
