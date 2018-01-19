fd340c6c7455f2952cdfe79e94a8d85a0acff861
Zack Patrick <zpatrick319@Hotmail.com>
2018-01-19 13:57:35 -0800

    update task controller

---
+++ api/controllers/task.go
@@ -0,0 +1,106 @@
+package controllers
+
+import (
+	"encoding/json"
+
+	"github.com/quintilesims/layer0/api/provider"
+	"github.com/quintilesims/layer0/common/errors"
+	"github.com/quintilesims/layer0/common/models"
+	"github.com/zpatrick/fireball"
+)
+
+type TaskController struct {
+	TaskProvider provider.TaskProvider
+}
+
+func NewTaskController(t provider.TaskProvider) *TaskController {
+	return &TaskController{
+		TaskProvider: t,
+	}
+}
+
+func (t *TaskController) Routes() []*fireball.Route {
+	return []*fireball.Route{
+		{
+			Path: "/task",
+			Handlers: fireball.Handlers{
+				"GET":  t.listTasks,
+				"POST": t.createTask,
+			},
+		},
+		{
+			Path: "/task/:id",
+			Handlers: fireball.Handlers{
+				"GET":    t.readTask,
+				"DELETE": t.deleteTask,
+			},
+		},
+		{
+			Path: "/task/:id/logs",
+			Handlers: fireball.Handlers{
+				"GET": t.readTaskLogs,
+			},
+		},
+	}
+}
+
+func (t *TaskController) createTask(c *fireball.Context) (fireball.Response, error) {
+	var req models.CreateTaskRequest
+	if err := json.NewDecoder(c.Request.Body).Decode(&req); err != nil {
+		return nil, errors.New(errors.InvalidRequest, err)
+	}
+
+	if err := req.Validate(); err != nil {
+		return nil, errors.New(errors.InvalidRequest, err)
+	}
+
+	taskID, err := t.TaskProvider.Create(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return fireball.NewJSONResponse(200, models.CreateEntityResponse{EntityID: taskID})
+}
+
+func (t *TaskController) deleteTask(c *fireball.Context) (fireball.Response, error) {
+	taskID := c.PathVariables["id"]
+	if err := t.TaskProvider.Delete(taskID); err != nil {
+		return nil, err
+	}
+
+	return fireball.NewJSONResponse(200, nil)
+}
+
+func (t *TaskController) listTasks(c *fireball.Context) (fireball.Response, error) {
+	tasks, err := t.TaskProvider.List()
+	if err != nil {
+		return nil, err
+	}
+
+	return fireball.NewJSONResponse(200, tasks)
+}
+
+func (t *TaskController) readTask(c *fireball.Context) (fireball.Response, error) {
+	taskID := c.PathVariables["id"]
+	task, err := t.TaskProvider.Read(taskID)
+	if err != nil {
+		return nil, err
+	}
+
+	return fireball.NewJSONResponse(200, task)
+}
+
+func (t *TaskController) readTaskLogs(c *fireball.Context) (fireball.Response, error) {
+	taskID := c.PathVariables["id"]
+	tail, start, end, err := parseLoggingQuery(c.Request.URL.Query())
+	if err != nil {
+		return nil, errors.New(errors.InvalidRequest, err)
+	}
+
+	logs, err := t.TaskProvider.Logs(taskID, tail, start, end)
+	if err != nil {
+		return nil, err
+	}
+
+	return fireball.NewJSONResponse(200, logs)
+}
---
+++ api/controllers/task_test.go
@@ -0,0 +1,192 @@
+package controllers
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"github.com/quintilesims/layer0/api/provider/mock_provider"
+	"github.com/quintilesims/layer0/common/models"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateTask(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockTaskProvider := mock_provider.NewMockTaskProvider(ctrl)
+	controller := NewTaskController(mockTaskProvider)
+
+	req := models.CreateTaskRequest{
+		TaskName:      "tsk_name",
+		EnvironmentID: "env_id",
+		DeployID:      "dpl_id",
+		ContainerOverrides: []models.ContainerOverride{
+			{ContainerName: "c1", EnvironmentOverrides: map[string]string{"k1": "v1"}},
+			{ContainerName: "c2", EnvironmentOverrides: map[string]string{"k2": "v2"}},
+		},
+	}
+
+	mockTaskProvider.EXPECT().
+		Create(req).
+		Return("tsk_id", nil)
+
+	c := newFireballContext(t, req, nil)
+	resp, err := controller.createTask(c)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var response models.CreateEntityResponse
+	recorder := unmarshalBody(t, resp, &response)
+
+	assert.Equal(t, 200, recorder.Code)
+	assert.Equal(t, "tsk_id", response.EntityID)
+}
+
+func TestDeleteTask(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockTaskProvider := mock_provider.NewMockTaskProvider(ctrl)
+	controller := NewTaskController(mockTaskProvider)
+
+	mockTaskProvider.EXPECT().
+		Delete("tsk_id").
+		Return(nil)
+
+	c := newFireballContext(t, nil, map[string]string{"id": "tsk_id"})
+	resp, err := controller.deleteTask(c)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	recorder := unmarshalBody(t, resp, nil)
+	assert.Equal(t, 200, recorder.Code)
+}
+
+func TestListTasks(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockTaskProvider := mock_provider.NewMockTaskProvider(ctrl)
+	controller := NewTaskController(mockTaskProvider)
+
+	expected := []models.TaskSummary{
+		{
+			TaskID:          "tsk_id1",
+			TaskName:        "tsk_name1",
+			EnvironmentID:   "env_id1",
+			EnvironmentName: "env_name1",
+		},
+		{
+			TaskID:          "tsk_id2",
+			TaskName:        "tskd_name2",
+			EnvironmentID:   "env_id2",
+			EnvironmentName: "env_name2",
+		},
+	}
+
+	mockTaskProvider.EXPECT().
+		List().
+		Return(expected, nil)
+
+	c := newFireballContext(t, nil, nil)
+	resp, err := controller.listTasks(c)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var response []models.TaskSummary
+	recorder := unmarshalBody(t, resp, &response)
+
+	assert.Equal(t, 200, recorder.Code)
+	assert.Equal(t, expected, response)
+}
+
+func TestReadTask(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	expected := models.Task{
+		TaskID:          "tsk_id",
+		TaskName:        "tsk_name",
+		EnvironmentID:   "env_id",
+		EnvironmentName: "env_name",
+		DeployID:        "dpl_id",
+		DeployName:      "dpl_name",
+		DeployVersion:   "1",
+		Status:          "RUNNING",
+		Containers: []models.Container{
+			{ContainerName: "c1", Status: "RUNNING", ExitCode: 0},
+			{ContainerName: "c2", Status: "STOPPED", ExitCode: 1},
+		},
+	}
+
+	mockTaskProvider := mock_provider.NewMockTaskProvider(ctrl)
+	controller := NewTaskController(mockTaskProvider)
+
+	mockTaskProvider.EXPECT().
+		Read("tsk_id").
+		Return(&expected, nil)
+
+	c := newFireballContext(t, nil, map[string]string{"id": "tsk_id"})
+	resp, err := controller.readTask(c)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var response models.Task
+	recorder := unmarshalBody(t, resp, &response)
+
+	assert.Equal(t, 200, recorder.Code)
+	assert.Equal(t, expected, response)
+}
+
+func TestReadTaskLogs(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockTaskProvider := mock_provider.NewMockTaskProvider(ctrl)
+	controller := NewTaskController(mockTaskProvider)
+
+	expected := []models.LogFile{
+		{
+			ContainerName: "apline",
+			Lines:         []string{"hello", "world"},
+		},
+	}
+
+	tail := "100"
+	start, err := time.Parse(TimeLayout, "2001-01-02 10:00")
+	if err != nil {
+		t.Fatalf("Failed to parse start: %v", err)
+	}
+
+	end, err := time.Parse(TimeLayout, "2001-01-02 12:00")
+	if err != nil {
+		t.Fatalf("Failed to parse end: %v", err)
+	}
+
+	mockTaskProvider.EXPECT().
+		Logs("tsk_id", 100, start, end).
+		Return(expected, nil)
+
+	c := newFireballContext(t, nil, map[string]string{"id": "tsk_id"})
+	c.Request.URL.RawQuery = fmt.Sprintf("tail=%s&start=%s&end=%s",
+		tail,
+		start.Format(TimeLayout),
+		end.Format(TimeLayout))
+
+	resp, err := controller.readTaskLogs(c)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var response []models.LogFile
+	recorder := unmarshalBody(t, resp, &response)
+
+	assert.Equal(t, 200, recorder.Code)
+	assert.Equal(t, expected, response)
+}
