12b75db0796b9d37da1eea2196a1bb64350199c9
Nick Hayward <nehayward@gmail.com>
2018-02-27 15:08:58 -0800

    Add packages for choco

--- packages/choco/tools/chocolateyinstall.ps1
+++ packages/choco/tools/chocolateyinstall.ps1
@@ -1,13 +1,9 @@
-﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
-#   $f='c:\path\to\thisFile.ps1'
-#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
-
-$ErrorActionPreference = 'Stop'; # stop on all errors
+﻿$ErrorActionPreference = 'Stop';
 
 
-$packageName= 'layer0' # arbitrary name for the package, used in messages
+$packageName= 'layer0'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/quintilesims/layer0/releases/download/v0.10.6/Windows.zip'
+$url        = 'https://github.com/quintilesims/layer0/releases/download/v0.10.7/Windows.zip'
 $fileLocation = Join-Path $toolsDir 'l0.exe'
 $unzipMethod = 'builtin'
 
@@ -19,16 +15,14 @@ $packageArgs = @{
   url64bit      = $url64
   file         = $fileLocation
 
-  #MSI
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 
-  # optional, highly recommended
-  softwareName  = 'layer0*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  softwareName  = 'layer0*'
   checksum      = ''
-  checksumType  = 'md5' #default is md5, can also be sha1, sha256 or sha512
+  checksumType  = 'md5'
   checksum64    = ''
-  checksumType64= 'md5' #default is checksumType
+  checksumType64= 'md5'
 }
 
 Install-ChocolateyZipPackage @packageArgs
