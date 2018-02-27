9e8694c5fb2e4c5d8434adbd3a8045582a610ed5
Nick Hayward <nehayward@gmail.com>
2018-02-27 11:00:29 -0800

    Add initial choco support

---
+++ packages/choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,34 @@
+﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
+#   $f='c:\path\to\thisFile.ps1'
+#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+
+$packageName= 'layer0' # arbitrary name for the package, used in messages
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://github.com/quintilesims/layer0/releases/download/v0.10.6/Windows.zip'
+$fileLocation = Join-Path $toolsDir 'l0.exe'
+$unzipMethod = 'builtin'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'EXE'
+  url           = $url
+  url64bit      = $url64
+  file         = $fileLocation
+
+  #MSI
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`"" # ALLUSERS=1 DISABLEDESKTOPSHORTCUT=1 ADDDESKTOPICON=0 ADDSTARTMENU=0
+  validExitCodes= @(0, 3010, 1641)
+
+  # optional, highly recommended
+  softwareName  = 'layer0*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+  checksum      = ''
+  checksumType  = 'md5' #default is md5, can also be sha1, sha256 or sha512
+  checksum64    = ''
+  checksumType64= 'md5' #default is checksumType
+}
+
+Install-ChocolateyZipPackage @packageArgs
