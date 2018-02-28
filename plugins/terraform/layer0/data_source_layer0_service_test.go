0ca5170201d2e775525eeec95af6a090e0bf9b94
Tanner Lake <tanner.lake@gmail.com>
2018-02-28 13:52:59 -0800

    Add service statefulness to terraform plugin

--- plugins/terraform/layer0/data_source_layer0_service_test.go
+++ plugins/terraform/layer0/data_source_layer0_service_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDataSourceLayer0ServiceRead(t *testing.T) {
+func TestDataSourceLayer0ServiceRead_stateless(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
@@ -29,6 +29,7 @@ func TestDataSourceLayer0ServiceRead(t *testing.T) {
 	service := &models.Service{
 		ServiceID:       "svc_id",
 		ServiceName:     "svc_name",
+		ServiceType:     models.DeployCompatibilityStateless,
 		EnvironmentID:   "env_id",
 		EnvironmentName: "env_name",
 		DesiredCount:    2,
@@ -53,4 +54,51 @@ func TestDataSourceLayer0ServiceRead(t *testing.T) {
 	assert.Equal(t, "env_id", d.Get("environment_id"))
 	assert.Equal(t, "env_name", d.Get("environment_name"))
 	assert.Equal(t, 2, d.Get("scale"))
+	assert.Equal(t, false, d.Get("stateful"))
+}
+
+func TestDataSourceLayer0ServiceRead_stateful(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockClient := mock_client.NewMockClient(ctrl)
+
+	query := url.Values{}
+	query.Set(models.TagQueryParamType, "service")
+	query.Set(models.TagQueryParamName, "svc_name")
+	query.Set(models.TagQueryParamEnvironmentID, "env_id")
+
+	mockClient.EXPECT().
+		ListTags(query).
+		Return([]models.Tag{{EntityID: "svc_id"}}, nil)
+
+	service := &models.Service{
+		ServiceID:       "svc_id",
+		ServiceName:     "svc_name",
+		ServiceType:     models.DeployCompatibilityStateful,
+		EnvironmentID:   "env_id",
+		EnvironmentName: "env_name",
+		DesiredCount:    2,
+	}
+
+	mockClient.EXPECT().
+		ReadService("svc_id").
+		Return(service, nil)
+
+	serviceDataSource := Provider().(*schema.Provider).DataSourcesMap["layer0_service"]
+	d := schema.TestResourceDataRaw(t, serviceDataSource.Schema, map[string]interface{}{
+		"name":           "svc_name",
+		"environment_id": "env_id",
+	})
+
+	if err := dataSourceLayer0ServiceRead(d, mockClient); err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "svc_id", d.Id())
+	assert.Equal(t, "svc_name", d.Get("name"))
+	assert.Equal(t, "env_id", d.Get("environment_id"))
+	assert.Equal(t, "env_name", d.Get("environment_name"))
+	assert.Equal(t, 2, d.Get("scale"))
+	assert.Equal(t, true, d.Get("stateful"))
 }
