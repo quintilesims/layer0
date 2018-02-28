8af0111f65921098fc5f08e32f5498571968656f
Tanner Lake <tanner.lake@gmail.com>
2018-02-28 12:13:45 -0800

    Add stateful and stateless paths in provider testing

--- api/provider/aws/test_aws/task_create_test.go
+++ api/provider/aws/test_aws/task_create_test.go
@@ -16,7 +16,152 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestTaskCreate_defaults(t *testing.T) {
+func TestTaskCreate_stateless(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockAWS := awsc.NewMockClient(ctrl)
+	tagStore := tag.NewMemoryStore()
+	mockConfig := mock_config.NewMockAPIConfig(ctrl)
+
+	mockConfig.EXPECT().Instance().Return("test").AnyTimes()
+	mockConfig.EXPECT().PrivateSubnets().Return([]string{"subnet-test"})
+
+	defer provider.SetEntityIDGenerator("tsk_id")()
+
+	tags := models.Tags{
+		{
+			EntityID:   "dpl_id",
+			EntityType: "deploy",
+			Key:        "name",
+			Value:      "dpl_name",
+		},
+		{
+			EntityID:   "dpl_id",
+			EntityType: "deploy",
+			Key:        "version",
+			Value:      "version",
+		},
+	}
+
+	for _, tag := range tags {
+		if err := tagStore.Insert(tag); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// define expected ec2.DescribeSecurityGroups
+	// (part of stateless workflow)
+	ec2Filter := &ec2.Filter{}
+	ec2Filter.SetName("group-name")
+	ec2Filter.SetValues([]*string{aws.String("l0-test-env_id-env")})
+
+	describeSecurityGroupsInput := &ec2.DescribeSecurityGroupsInput{}
+	describeSecurityGroupsInput.SetFilters([]*ec2.Filter{ec2Filter})
+
+	securityGroup := &ec2.SecurityGroup{}
+	securityGroup.SetGroupName("l0-test-env_id-env")
+	securityGroup.SetGroupId("sg-test")
+
+	securityGroups := []*ec2.SecurityGroup{securityGroup}
+
+	describeSecurityGroupsOutput := &ec2.DescribeSecurityGroupsOutput{}
+	describeSecurityGroupsOutput.SetSecurityGroups(securityGroups)
+
+	mockAWS.EC2.EXPECT().
+		DescribeSecurityGroups(describeSecurityGroupsInput).
+		Return(describeSecurityGroupsOutput, nil)
+
+	// define expected RunTask
+	kvp := &ecs.KeyValuePair{}
+	kvp.SetName("key")
+	kvp.SetValue("val")
+
+	override := &ecs.ContainerOverride{}
+	override.SetEnvironment([]*ecs.KeyValuePair{kvp})
+	override.SetName("container")
+
+	taskOverride := &ecs.TaskOverride{}
+	taskOverride.SetContainerOverrides([]*ecs.ContainerOverride{override})
+
+	awsvpcConfig := &ecs.AwsVpcConfiguration{}
+	awsvpcConfig.SetAssignPublicIp(ecs.AssignPublicIpDisabled)
+	awsvpcConfig.SetSecurityGroups([]*string{aws.String("sg-test")})
+	awsvpcConfig.SetSubnets([]*string{aws.String("subnet-test")})
+
+	networkConfig := &ecs.NetworkConfiguration{}
+	networkConfig.SetAwsvpcConfiguration(awsvpcConfig)
+
+	runTaskInput := &ecs.RunTaskInput{}
+	runTaskInput.SetCluster("l0-test-env_id")
+	runTaskInput.SetLaunchType(ecs.LaunchTypeFargate)
+	runTaskInput.SetNetworkConfiguration(networkConfig)
+	runTaskInput.SetOverrides(taskOverride)
+	runTaskInput.SetPlatformVersion(config.DefaultFargatePlatformVersion)
+	runTaskInput.SetStartedBy("test")
+	runTaskInput.SetTaskDefinition("l0-test-dpl_name:version")
+
+	task := &ecs.Task{}
+	task.SetTaskArn("arn:aws:ecs:region:012345678910:task/arn")
+
+	runTaskOutput := &ecs.RunTaskOutput{}
+	runTaskOutput.SetTasks([]*ecs.Task{task})
+
+	mockAWS.ECS.EXPECT().
+		RunTask(runTaskInput).
+		Return(runTaskOutput, nil)
+
+	// define request
+	containerOverride := []models.ContainerOverride{
+		{
+			ContainerName:        "container",
+			EnvironmentOverrides: map[string]string{"key": "val"},
+		},
+	}
+
+	req := models.CreateTaskRequest{
+		ContainerOverrides: containerOverride,
+		DeployID:           "dpl_id",
+		EnvironmentID:      "env_id",
+		TaskName:           "tsk_name",
+		TaskType:           models.DeployCompatibilityStateless,
+	}
+
+	target := provider.NewTaskProvider(mockAWS.Client(), tagStore, mockConfig)
+	result, err := target.Create(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, "tsk_id", result)
+
+	expectedTags := models.Tags{
+		{
+			EntityID:   "tsk_id",
+			EntityType: "task",
+			Key:        "name",
+			Value:      "tsk_name",
+		},
+		{
+			EntityID:   "tsk_id",
+			EntityType: "task",
+			Key:        "environment_id",
+			Value:      "env_id",
+		},
+		{
+			EntityID:   "tsk_id",
+			EntityType: "task",
+			Key:        "arn",
+			Value:      "arn:aws:ecs:region:012345678910:task/arn",
+		},
+	}
+
+	for _, tag := range expectedTags {
+		assert.Contains(t, tagStore.Tags(), tag)
+	}
+}
+
+func TestTaskCreate_stateful(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
 
@@ -49,26 +194,44 @@ func TestTaskCreate_defaults(t *testing.T) {
 		}
 	}
 
-	ec2Filter := &ec2.Filter{}
-	ec2Filter.SetName("group-name")
-	ec2Filter.SetValues([]*string{aws.String("l0-test-env_id-env")})
+	// define expected RunTask
+	kvp := &ecs.KeyValuePair{}
+	kvp.SetName("key")
+	kvp.SetValue("val")
 
-	describeSecurityGroupsInput := &ec2.DescribeSecurityGroupsInput{}
-	describeSecurityGroupsInput.SetFilters([]*ec2.Filter{ec2Filter})
+	override := &ecs.ContainerOverride{}
+	override.SetEnvironment([]*ecs.KeyValuePair{kvp})
+	override.SetName("container")
 
-	securityGroup := &ec2.SecurityGroup{}
-	securityGroup.SetGroupName("l0-test-env_id-env")
-	securityGroup.SetGroupId("sg-test")
-	securityGroups := []*ec2.SecurityGroup{securityGroup}
-	describeSecurityGroupsOutput := &ec2.DescribeSecurityGroupsOutput{}
-	describeSecurityGroupsOutput.SetSecurityGroups(securityGroups)
+	taskOverride := &ecs.TaskOverride{}
+	taskOverride.SetContainerOverrides([]*ecs.ContainerOverride{override})
 
-	mockAWS.EC2.EXPECT().
-		DescribeSecurityGroups(describeSecurityGroupsInput).
-		Return(describeSecurityGroupsOutput, nil)
+	awsvpcConfig := &ecs.AwsVpcConfiguration{}
+	awsvpcConfig.SetAssignPublicIp(ecs.AssignPublicIpDisabled)
+	awsvpcConfig.SetSecurityGroups([]*string{aws.String("sg-test")})
+	awsvpcConfig.SetSubnets([]*string{aws.String("subnet-test")})
 
-	mockConfig.EXPECT().PrivateSubnets().Return([]string{"subnet-test"})
+	networkConfig := &ecs.NetworkConfiguration{}
+	networkConfig.SetAwsvpcConfiguration(awsvpcConfig)
 
+	runTaskInput := &ecs.RunTaskInput{}
+	runTaskInput.SetCluster("l0-test-env_id")
+	runTaskInput.SetLaunchType(ecs.LaunchTypeEc2)
+	runTaskInput.SetOverrides(taskOverride)
+	runTaskInput.SetStartedBy("test")
+	runTaskInput.SetTaskDefinition("l0-test-dpl_name:version")
+
+	task := &ecs.Task{}
+	task.SetTaskArn("arn:aws:ecs:region:012345678910:task/arn")
+
+	runTaskOutput := &ecs.RunTaskOutput{}
+	runTaskOutput.SetTasks([]*ecs.Task{task})
+
+	mockAWS.ECS.EXPECT().
+		RunTask(runTaskInput).
+		Return(runTaskOutput, nil)
+
+	// define request
 	containerOverride := []models.ContainerOverride{
 		{
 			ContainerName:        "container",
@@ -81,45 +244,8 @@ func TestTaskCreate_defaults(t *testing.T) {
 		DeployID:           "dpl_id",
 		EnvironmentID:      "env_id",
 		TaskName:           "tsk_name",
-		TaskType:           models.DeployCompatibilityStateless,
+		TaskType:           models.DeployCompatibilityStateful,
 	}
-
-	kvp := &ecs.KeyValuePair{}
-	kvp.SetName("key")
-	kvp.SetValue("val")
-
-	override := &ecs.ContainerOverride{}
-	override.SetName("container")
-	override.SetEnvironment([]*ecs.KeyValuePair{kvp})
-
-	taskOverride := &ecs.TaskOverride{}
-	taskOverride.SetContainerOverrides([]*ecs.ContainerOverride{override})
-
-	awsvpcConfig := &ecs.AwsVpcConfiguration{}
-	awsvpcConfig.SetAssignPublicIp(ecs.AssignPublicIpDisabled)
-	awsvpcConfig.SetSecurityGroups([]*string{aws.String("sg-test")})
-	awsvpcConfig.SetSubnets([]*string{aws.String("subnet-test")})
-	networkConfig := &ecs.NetworkConfiguration{}
-	networkConfig.SetAwsvpcConfiguration(awsvpcConfig)
-
-	runTaskInput := &ecs.RunTaskInput{}
-	runTaskInput.SetCluster("l0-test-env_id")
-	runTaskInput.SetLaunchType(ecs.LaunchTypeFargate)
-	runTaskInput.SetStartedBy("test")
-	runTaskInput.SetTaskDefinition("l0-test-dpl_name:version")
-	runTaskInput.SetOverrides(taskOverride)
-	runTaskInput.SetNetworkConfiguration(networkConfig)
-	runTaskInput.SetPlatformVersion(config.DefaultFargatePlatformVersion)
-
-	task := &ecs.Task{}
-	task.SetTaskArn("arn:aws:ecs:region:012345678910:task/arn")
-
-	runTaskOutput := &ecs.RunTaskOutput{}
-	runTaskOutput.SetTasks([]*ecs.Task{task})
-
-	mockAWS.ECS.EXPECT().
-		RunTask(runTaskInput).
-		Return(runTaskOutput, nil)
 
 	target := provider.NewTaskProvider(mockAWS.Client(), tagStore, mockConfig)
 	result, err := target.Create(req)
