58ca8c0b0b837f20230bce52b98eec8e76e5d8a9
Nick Hayward <nehayward@gmail.com>
2018-03-05 10:21:50 -0800

    Add homebrew and choco support

--- packages/choco/README.md
+++ packages/choco/README.md
@@ -1,2 +1,10 @@
+## Install
+* `choco pack` in .nuspec dir
+* `choco install -f packagename.nupkg -s .`
+
+## Requirements 
+* Windows OS
+* Choco
+
 ## Resources
 [Helpful Doc](https://github.com/chocolatey/choco/wiki/CreatePackagesQuickStart)
--- packages/choco/tools/chocolateyinstall.ps1
+++ packages/choco/tools/chocolateyinstall.ps1
@@ -4,7 +4,6 @@
 $packageName= 'layer0'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://github.com/quintilesims/layer0/releases/download/v0.10.7/Windows.zip'
-$fileLocation = Join-Path $toolsDir 'l0.exe'
 $unzipMethod = 'builtin'
 
 $packageArgs = @{
@@ -12,17 +11,10 @@ $packageArgs = @{
   unzipLocation = $toolsDir
   fileType      = 'EXE'
   url           = $url
-  url64bit      = $url64
-  file         = $fileLocation
-
-  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
-  validExitCodes= @(0, 3010, 1641)
 
   softwareName  = 'layer0*'
-  checksum      = ''
+  checksum      = '2B86012BE3B71189E012DDCA32D39BE3'
   checksumType  = 'md5'
-  checksum64    = ''
-  checksumType64= 'md5'
 }
 
 Install-ChocolateyZipPackage @packageArgs
