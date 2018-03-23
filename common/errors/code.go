f0bc46457e7151b760efc9309066e21023b7380c
Zack Patrick <zpatrick319@Hotmail.com>
2018-03-23 15:21:30 -0700

    use functional options for retry pkg

--- common/errors/code.go
+++ common/errors/code.go
@@ -16,5 +16,5 @@ const (
 	ServiceDoesNotExist      ErrorCode = "ServiceDoesNotExist"
 	TaskDoesNotExist         ErrorCode = "TaskDoesNotExist"
 	UnexpectedError          ErrorCode = "UnexpectedError"
-	FailedRequestTimeout     ErrorCode = "FailedRequestTimeout"
+	MaximumRetriesReached    ErrorCode = "MaximumRetriesReached"
 )
