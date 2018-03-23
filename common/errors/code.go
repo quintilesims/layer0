b37ace6ab9d32068119dae9dcafc265b0533256d
Zack Patrick <zpatrick319@Hotmail.com>
2018-03-23 15:24:09 -0700

    keep timeout error code

--- common/errors/code.go
+++ common/errors/code.go
@@ -16,5 +16,5 @@ const (
 	ServiceDoesNotExist      ErrorCode = "ServiceDoesNotExist"
 	TaskDoesNotExist         ErrorCode = "TaskDoesNotExist"
 	UnexpectedError          ErrorCode = "UnexpectedError"
-	MaximumRetriesReached    ErrorCode = "MaximumRetriesReached"
+	FailedRequestTimeout     ErrorCode = "FailedRequestTimeout"
 )
