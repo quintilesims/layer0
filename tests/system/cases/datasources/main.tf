ee5569cf245f0b4b8d4473646dcdf03007b763b4
Tanner Lake <tanner.lake@gmail.com>
2018-04-03 15:45:35 -0700

    Update datasources test to test both stateless and stateful deployments

--- tests/system/cases/datasources/main.tf
+++ tests/system/cases/datasources/main.tf
@@ -8,8 +8,8 @@ resource "layer0_environment" "datasources" {
   name = "dsrctest"
 }
 
-resource "layer0_load_balancer" "datasources" {
-  name        = "dsrctest"
+resource "layer0_load_balancer" "datasources_stateless" {
+  name        = "dsrctest_stateless"
   environment = "${layer0_environment.datasources.id}"
 
   port {
@@ -19,17 +19,41 @@ resource "layer0_load_balancer" "datasources" {
   }
 }
 
-resource "layer0_service" "datasources" {
-  name          = "dsrctest"
+resource "layer0_service" "datasources_stateless" {
+  name          = "dsrctest_stateless"
   environment   = "${layer0_environment.datasources.id}"
-  deploy        = "${layer0_deploy.datasources.id}"
-  load_balancer = "${layer0_load_balancer.datasources.id}"
-  scale         = "1"
+  deploy        = "${layer0_deploy.datasources_stateless.id}"
+  load_balancer = "${layer0_load_balancer.datasources_stateless.id}"
 }
 
-resource "layer0_deploy" "datasources" {
-  name    = "dsrctest"
-  content = "${file("${path.module}/Dockerrun.aws.json")}"
+resource "layer0_deploy" "datasources_stateless" {
+  name    = "dsrctest_stateless"
+  content = "${file("${path.module}/stateless.dockerrun.aws.json")}"
+}
+
+resource "layer0_load_balancer" "datasources_stateful" {
+  name        = "dsrctest_stateful"
+  environment = "${layer0_environment.datasources.id}"
+  type        = "classic"
+
+  port {
+    host_port      = 80
+    container_port = 80
+    protocol       = "http"
+  }
+}
+
+resource "layer0_service" "datasources_stateful" {
+  name          = "dsrctest_stateful"
+  environment   = "${layer0_environment.datasources.id}"
+  deploy        = "${layer0_deploy.datasources_stateful.id}"
+  load_balancer = "${layer0_load_balancer.datasources_stateful.id}"
+  stateful      = true
+}
+
+resource "layer0_deploy" "datasources_stateful" {
+  name    = "dsrctest_stateful"
+  content = "${file("${path.module}/stateful.dockerrun.aws.json")}"
 }
 
 data "layer0_environment" "datasources" {
@@ -37,20 +61,38 @@ data "layer0_environment" "datasources" {
   name       = "${layer0_environment.datasources.name}"
 }
 
-data "layer0_deploy" "datasources" {
-  depends_on = ["layer0_deploy.datasources"]
-  name       = "${layer0_deploy.datasources.name}"
-  version    = "${layer0_deploy.datasources.version}"
+data "layer0_deploy" "datasources_stateless" {
+  depends_on = ["layer0_deploy.datasources_stateless"]
+  name       = "${layer0_deploy.datasources_stateless.name}"
+  version    = "${layer0_deploy.datasources_stateless.version}"
 }
 
-data "layer0_service" "datasources" {
-  depends_on     = ["layer0_service.datasources"]
-  name           = "${layer0_service.datasources.name}"
-  environment_id = "${layer0_environment.datasources.id}"
+data "layer0_service" "datasources_stateless" {
+  depends_on     = ["layer0_service.datasources_stateless"]
+  name           = "${layer0_service.datasources_stateless.name}"
+  environment_id = "${layer0_environment.datasources_stateless.id}"
 }
 
-data "layer0_load_balancer" "datasources" {
-  depends_on     = ["layer0_load_balancer.datasources", "layer0_service.datasources"]
-  name           = "${layer0_load_balancer.datasources.name}"
-  environment_id = "${layer0_environment.datasources.id}"
+data "layer0_load_balancer" "datasources_stateless" {
+  depends_on     = ["layer0_load_balancer.datasources_stateless", "layer0_service.datasources_stateless"]
+  name           = "${layer0_load_balancer.datasources_stateless.name}"
+  environment_id = "${layer0_environment.datasources_stateless.id}"
+}
+
+data "layer0_deploy" "datasources_stateful" {
+  depends_on = ["layer0_deploy.datasources_stateful"]
+  name       = "${layer0_deploy.datasources_stateful.name}"
+  version    = "${layer0_deploy.datasources_stateful.version}"
+}
+
+data "layer0_service" "datasources_stateful" {
+  depends_on     = ["layer0_service.datasources_stateful"]
+  name           = "${layer0_service.datasources_stateful.name}"
+  environment_id = "${layer0_environment.datasources_stateful.id}"
+}
+
+data "layer0_load_balancer" "datasources_stateful" {
+  depends_on     = ["layer0_load_balancer.datasources_stateful", "layer0_service.datasources_stateful"]
+  name           = "${layer0_load_balancer.datasources_stateful.name}"
+  environment_id = "${layer0_environment.datasources_stateful.id}"
 }
--- tests/system/cases/datasources/outputs.tf
+++ tests/system/cases/datasources/outputs.tf
@@ -38,110 +38,246 @@ output "environment_ami_expected" {
 
 # -- Deploy --
 
-output "deploy_id" {
-  value = "${data.layer0_deploy.datasources.id}"
+output "stateless_deploy_id" {
+  value = "${data.layer0_deploy.datasources_stateless.id}"
 }
 
-output "deploy_id_expected" {
-  value = "${layer0_deploy.datasources.id}"
+output "stateless_deploy_id_expected" {
+  value = "${layer0_deploy.datasources_stateless.id}"
 }
 
-output "deploy_name" {
-  value = "${data.layer0_deploy.datasources.version}"
+output "stateless_deploy_name" {
+  value = "${data.layer0_deploy.datasources_stateless.version}"
 }
 
-output "deploy_name_expected" {
-  value = "${layer0_deploy.datasources.version}"
+output "stateless_deploy_name_expected" {
+  value = "${layer0_deploy.datasources_stateless.version}"
 }
 
-output "deploy_version" {
-  value = "${data.layer0_deploy.datasources.version}"
+output "stateless_deploy_version" {
+  value = "${data.layer0_deploy.datasources_stateless.version}"
 }
 
-output "deploy_version_expected" {
-  value = "${layer0_deploy.datasources.version}"
+output "stateless_deploy_version_expected" {
+  value = "${layer0_deploy.datasources_stateless.version}"
+}
+
+output "stateful_deploy_id" {
+  value = "${data.layer0_deploy.datasources_stateful.id}"
+}
+
+output "stateful_deploy_id_expected" {
+  value = "${layer0_deploy.datasources_stateful.id}"
+}
+
+output "stateful_deploy_name" {
+  value = "${data.layer0_deploy.datasources_stateful.version}"
+}
+
+output "stateful_deploy_name_expected" {
+  value = "${layer0_deploy.datasources_stateful.version}"
+}
+
+output "stateful_deploy_version" {
+  value = "${data.layer0_deploy.datasources_stateful.version}"
+}
+
+output "stateful_deploy_version_expected" {
+  value = "${layer0_deploy.datasources_stateful.version}"
 }
 
 # -- Load Balancer --
 
-output "load_balancer_id" {
-  value = "${data.layer0_load_balancer.datasources.id}"
+output "stateless_load_balancer_id" {
+  value = "${data.layer0_load_balancer.datasources_stateless.id}"
 }
 
-output "load_balancer_id_expected" {
-  value = "${layer0_load_balancer.datasources.id}"
+output "stateless_load_balancer_id_expected" {
+  value = "${layer0_load_balancer.datasources_stateless.id}"
 }
 
-output "load_balancer_name" {
-  value = "${data.layer0_load_balancer.datasources.name}"
+output "stateless_load_balancer_name" {
+  value = "${data.layer0_load_balancer.datasources_stateless.name}"
 }
 
-output "load_balancer_name_expected" {
-  value = "${layer0_load_balancer.datasources.name}"
+output "stateless_load_balancer_name_expected" {
+  value = "${layer0_load_balancer.datasources_stateless.name}"
 }
 
-output "load_balancer_environment_name" {
-  value = "${data.layer0_load_balancer.datasources.environment_name}"
+output "stateless_load_balancer_environment_name" {
+  value = "${data.layer0_load_balancer.datasources_stateless.environment_name}"
 }
 
-output "load_balancer_environment_name_expected" {
+output "stateless_load_balancer_environment_name_expected" {
   value = "${layer0_environment.datasources.name}"
 }
 
-output "load_balancer_private" {
-  value = "${data.layer0_load_balancer.datasources.private}"
+output "stateless_load_balancer_private" {
+  value = "${data.layer0_load_balancer.datasources_stateless.private}"
 }
 
-output "load_balancer_private_expected" {
-  value = "${layer0_load_balancer.datasources.private}"
+output "stateless_load_balancer_private_expected" {
+  value = "${layer0_load_balancer.datasources_stateless.private}"
 }
 
-output "load_balancer_url" {
-  value = "${data.layer0_load_balancer.datasources.url}"
+output "stateless_load_balancer_url" {
+  value = "${data.layer0_load_balancer.datasources_stateless.url}"
 }
 
-output "load_balancer_url_expected" {
-  value = "${layer0_load_balancer.datasources.url}"
+output "stateless_load_balancer_url_expected" {
+  value = "${layer0_load_balancer.datasources_stateless.url}"
+}
+
+output "stateless_load_balancer_type" {
+    value = "${data.layer0_load_balancer.datasources_stateless.type}"
+}
+
+output "stateless_load_balancer_type_expected" {
+    value = "${layer0_load_balancer.datasources_stateless.type}"
+}
+
+output "stateful_load_balancer_id" {
+  value = "${data.layer0_load_balancer.datasources_stateful.id}"
+}
+
+output "stateful_load_balancer_id_expected" {
+  value = "${layer0_load_balancer.datasources_stateful.id}"
+}
+
+output "stateful_load_balancer_name" {
+  value = "${data.layer0_load_balancer.datasources_stateful.name}"
+}
+
+output "stateful_load_balancer_name_expected" {
+  value = "${layer0_load_balancer.datasources_stateful.name}"
+}
+
+output "stateful_load_balancer_environment_name" {
+  value = "${data.layer0_load_balancer.datasources_stateful.environment_name}"
+}
+
+output "stateful_load_balancer_environment_name_expected" {
+  value = "${layer0_environment.datasources.name}"
+}
+
+output "stateful_load_balancer_private" {
+  value = "${data.layer0_load_balancer.datasources_stateful.private}"
+}
+
+output "stateful_load_balancer_private_expected" {
+  value = "${layer0_load_balancer.datasources_stateful.private}"
+}
+
+output "stateful_load_balancer_url" {
+  value = "${data.layer0_load_balancer.datasources_stateful.url}"
+}
+
+output "stateful_load_balancer_url_expected" {
+  value = "${layer0_load_balancer.datasources_stateful.url}"
+}
+
+output "stateful_load_balancer_type" {
+    value = "${data.layer0_load_balancer.datasources_stateful.type}"
+}
+
+output "stateful_load_balancer_type_expected" {
+    value = "${layer0_load_balancer.datasources_stateful.type}"
 }
 
 # -- Service --
 
-output "service_id" {
-  value = "${data.layer0_service.datasources.id}"
+output "stateless_service_id" {
+  value = "${data.layer0_service.datasources_stateless.id}"
 }
 
-output "service_id_expected" {
-  value = "${layer0_service.datasources.id}"
+output "stateless_service_id_expected" {
+  value = "${layer0_service.datasources_stateless.id}"
 }
 
-output "service_name" {
-  value = "${data.layer0_service.datasources.name}"
+output "stateless_service_name" {
+  value = "${data.layer0_service.datasources_stateless.name}"
 }
 
-output "service_name_expected" {
-  value = "${layer0_service.datasources.name}"
+output "stateless_service_name_expected" {
+  value = "${layer0_service.datasources_stateless.name}"
 }
 
-output "service_environment_id" {
-  value = "${data.layer0_service.datasources.environment_id}"
+output "stateless_service_environment_id" {
+  value = "${data.layer0_service.datasources_stateless.environment_id}"
 }
 
-output "service_environment_id_expected" {
+output "stateless_service_environment_id_expected" {
   value = "${layer0_environment.datasources.id}"
 }
 
-output "service_environment_name" {
-  value = "${data.layer0_service.datasources.environment_name}"
+output "stateless_service_environment_name" {
+  value = "${data.layer0_service.datasources_stateless.environment_name}"
 }
 
-output "service_environment_name_expected" {
+output "stateless_service_environment_name_expected" {
   value = "${layer0_environment.datasources.name}"
 }
 
-output "service_scale" {
-  value = "${data.layer0_service.datasources.scale}"
+output "stateless_service_scale" {
+  value = "${data.layer0_service.datasources_stateless.scale}"
 }
 
-output "service_scale_expected" {
-  value = "${layer0_service.datasources.scale}"
+output "stateless_service_scale_expected" {
+  value = "${layer0_service.datasources_stateless.scale}"
+}
+
+output "stateless_service_stateful" {
+  value = "${data.layer0_service.datasources_stateless.stateful}"
+}
+
+output "stateless_service_stateful_expected" {
+  value = "${layer0_service.datasources_stateless.stateful}"
+}
+
+output "stateful_service_id" {
+  value = "${data.layer0_service.datasources_stateful.id}"
+}
+
+output "stateful_service_id_expected" {
+  value = "${layer0_service.datasources_stateful.id}"
+}
+
+output "stateful_service_name" {
+  value = "${data.layer0_service.datasources_stateful.name}"
+}
+
+output "stateful_service_name_expected" {
+  value = "${layer0_service.datasources_stateful.name}"
+}
+
+output "stateful_service_environment_id" {
+  value = "${data.layer0_service.datasources_stateful.environment_id}"
+}
+
+output "stateful_service_environment_id_expected" {
+  value = "${layer0_environment.datasources.id}"
+}
+
+output "stateful_service_environment_name" {
+  value = "${data.layer0_service.datasources_stateful.environment_name}"
+}
+
+output "stateful_service_environment_name_expected" {
+  value = "${layer0_environment.datasources.name}"
+}
+
+output "stateful_service_scale" {
+  value = "${data.layer0_service.datasources_stateful.scale}"
+}
+
+output "stateful_service_scale_expected" {
+  value = "${layer0_service.datasources_stateful.scale}"
+}
+
+output "stateful_service_stateful" {
+  value = "${data.layer0_service.datasources_stateful.stateful}"
+}
+
+output "stateful_service_stateful_expected" {
+  value = "${layer0_service.datasources_stateful.stateful}"
 }
