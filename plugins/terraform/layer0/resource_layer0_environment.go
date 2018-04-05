4aefc17b1a2247aa7ec4a146aace044c9f080e82
Chemudugunta, Seshi <seshi.chemudugunta@us.imshealth.com>
2018-04-05 13:54:26 -0700

    Fixed issue with tf environment resource getting re-created.

--- plugins/terraform/layer0/resource_layer0_environment.go
+++ plugins/terraform/layer0/resource_layer0_environment.go
@@ -28,6 +28,7 @@ func resourceLayer0Environment() *schema.Resource {
 			"instance_type": {
 				Type:     schema.TypeString,
 				Optional: true,
+				Default:  config.DefaultEnvironmentInstanceType,
 				ForceNew: true,
 			},
 			"scale": {
