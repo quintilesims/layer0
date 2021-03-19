b9141717993fe49015f73719ea6419b275f0df49
Victor Bi <vbi@nz.imshealth.com>
2021-03-19 16:50:31 +1300

    added root as well.

--- setup/module/api/environment.tf
+++ setup/module/api/environment.tf
@@ -41,8 +41,16 @@ resource "aws_launch_configuration" "api" {
   user_data            = "${data.template_file.user_data.rendered}"
   key_name             = "${var.ssh_key_pair}"
   ebs_optimized        = true
-  volume_type          = "gp2"
-  volume_size          = "22"
+
+  root_block_device {
+    volume_type        = "gp2"
+    volume_size        = "8"
+  }
+  ebs_block_device {
+    device_name        = "l0-${var.name}-api-ebs"
+    volume_type        = "gp2"
+    volume_size        = "22"
+  }
 
   lifecycle {
     create_before_destroy = true
