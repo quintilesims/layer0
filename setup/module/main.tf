da76294f9e43e6710bebff64514093ad72820bef
Victor Bi <vbi@nz.imshealth.com>
2021-03-22 15:28:50 +1300

    update to aws 3.30

--- setup/module/main.tf
+++ setup/module/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  version    = ">= 2.0.0, <= 3.33.0"
+  version    = ">= 3.30.0, <= 3.33.0"
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
   region     = "${var.region}"
